3072c56bc6fc6d0d9f8484279238a7d241590cf2
Daniel Thorpe <danthorpe@me.com>
2015-06-26 15:51:14 +0100

    [OPR-1]: Creates framework project.

---
+++ Operations/Operations/Operations.h
@@ -0,0 +1,19 @@
+//
+//  Operations.h
+//  Operations
+//
+//  Created by Daniel Thorpe on 26/06/2015.
+//  Copyright (c) 2015 Daniel Thorpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Operations.
+FOUNDATION_EXPORT double OperationsVersionNumber;
+
+//! Project version string for Operations.
+FOUNDATION_EXPORT const unsigned char OperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Operations/PublicHeader.h>
+
+
