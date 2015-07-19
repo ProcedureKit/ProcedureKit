7d6f79b77c7bae440c9f48193918e3813b27f282
Daniel Thorpe <danthorpe@me.com>
2015-07-20 00:06:37 +0100

    [OPR-10]: Adds Cloud condition file.

---
+++ Operations/Conditions/Permissions/CloudCondition.swift
@@ -0,0 +1,13 @@
+//
+//  CloudCondition.swift
+//  Operations
+//
+//  Created by Daniel Thorpe on 20/07/2015.
+//  Copyright © 2015 Daniel Thorpe. All rights reserved.
+//
+
+import Foundation
+
+
+
+
