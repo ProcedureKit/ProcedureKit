1aea27fa8a5e93ad6ba9bf4e72e2c710607ab319
Daniel Thorpe <danthorpe@me.com>
2015-07-24 21:28:46 +0100

    [OPR-34]: Creates ReachableOperation files.

---
+++ framework/Operations/Operations/ReachableOperation.swift
@@ -0,0 +1,12 @@
+//
+//  ReachableOperation.swift
+//  Operations
+//
+//  Created by Daniel Thorpe on 24/07/2015.
+//  Copyright (c) 2015 Daniel Thorpe. All rights reserved.
+//
+
+import Foundation
+
+
+
---
+++ framework/OperationsTests/ReachableOperationTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReachableOperationTests.swift
+//  Operations
+//
+//  Created by Daniel Thorpe on 24/07/2015.
+//  Copyright (c) 2015 Daniel Thorpe. All rights reserved.
+//
+
+import XCTest
+import Operations
+
+class ReachableOperationTests: OperationTests {
+
+
+}
+
