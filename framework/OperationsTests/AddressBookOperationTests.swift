c596c8a5ba3b59d0bfd6beb1b927cd0d7d6a7781
Daniel Thorpe <danthorpe@me.com>
2015-07-27 16:55:38 +0100

    [OPR-36]: Adds initial test for AddressBook operation

---
+++ framework/OperationsTests/AddressBookOperationTests.swift
@@ -0,0 +1,67 @@
+//
+//  BlockConditionTests.swift
+//  Operations
+//
+//  Created by Daniel Thorpe on 20/07/2015.
+//  Copyright © 2015 Daniel Thorpe. All rights reserved.
+//
+
+import XCTest
+import AddressBook
+import Operations
+
+class TestableAddressBookManager: AddressBookAuthenticationManager {
+
+    var didAccessStatus = false
+    var didCreateAddressBook = false
+    var didRequestAccess = false
+
+    var status: ABAuthorizationStatus
+
+    var requestShouldSucceed = true
+    var addressBook: CFTypeRef! = nil
+    var error: CFErrorRef! = nil
+
+    init(_ s: ABAuthorizationStatus) {
+        status = s
+    }
+
+    func createAddressBook() -> (ABAddressBookRef!, CFErrorRef!) {
+        didCreateAddressBook = true
+
+        if let error = error {
+            return (nil, error)
+        }
+        return (addressBook as ABAddressBookRef, nil)
+    }
+
+    func requestAccessToAddressBook(addressBook: ABAddressBookRef, completion: ABAddressBookRequestAccessCompletionHandler) {
+        didRequestAccess = true
+        completion(requestShouldSucceed, error)
+    }
+}
+
+class AddressBookOperationTests: OperationTests {
+
+    func test__when_authorized_handler_receives_addressbook() {
+
+        var didReceiveAddressBook = false
+        let manager = TestableAddressBookManager(.Authorized)
+        manager.addressBook = "I'm posing as an Address Book Ref!" as CFTypeRef
+
+        let operation = AddressBookOperation(manager: manager, silent: false) { (addressBook, continuation) in
+            if let posedAddressBook = addressBook as? String {
+                didReceiveAddressBook = true
+            }
+            continuation()
+        }
+
+        addCompletionBlockToTestOperation(operation, withExpectation: expectationWithDescription("Test: \(__FUNCTION__)"))
+
+        runOperation(operation)
+        waitForExpectationsWithTimeout(5, handler: nil)
+
+        XCTAssertTrue(didReceiveAddressBook)
+    }
+
+}
