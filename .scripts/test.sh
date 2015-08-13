448fbf92edc7d89302b4397697d9b8c79971da96
Daniel Thorpe <danthorpe@me.com>
2015-08-13 12:48:29 +0100

    [swift_2.0]: Rewrites test script to use fastlane.

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
+bundle update && bundle exec fastlane test
 
-bundle update
-set -o pipefail && bundle exec xcodebuild -project "Operations.xcodeproj" -scheme "Operations" -destination "platform=iOS Simulator,name=iPhone 6,OS=8.4" test | xcpretty -c
+#bundle update
+#set -o pipefail && bundle exec xcodebuild -project "Operations.xcodeproj" -scheme "Operations" -destination "platform=iOS Simulator,name=iPhone 6,OS=8.4" test | xcpretty -c
