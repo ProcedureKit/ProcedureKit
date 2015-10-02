b9e1890430128c4252aad6d24597ee504a7c4497
Daniel Thorpe <danthorpe@me.com>
2015-10-02 23:53:40 +0100

    [OPR-97]: Fixes test errors due to changes.

--- OperationsTests/Core/LoggingObserverTests.swift
+++ OperationsTests/Core/LoggingObserverTests.swift
@@ -33,7 +33,7 @@ class LoggingObserverTests: OperationTests {
 
     func test__logger_outputs_number_of_received_errors() {
 
-        let operation = TestOperation(error: CloudContainerCondition.Error.NotAuthenticated)
+        let operation = TestOperation(error: BlockCondition.Error.BlockConditionFailed)
         addCompletionBlockToTestOperation(operation, withExpectation: expectationWithDescription("Test: \(__FUNCTION__)"))
 
         var receivedMessages = [String]()
@@ -46,14 +46,14 @@ class LoggingObserverTests: OperationTests {
         waitForExpectationsWithTimeout(3, handler: nil)
 
         XCTAssertEqual(receivedMessages.count, 2)
-        XCTAssertEqual(receivedMessages[1], "\(operation): finished with error(s): [Operations.CloudContainerCondition.Error.NotAuthenticated].")
+        XCTAssertEqual(receivedMessages[1], "\(operation): finished with error(s): [Operations.BlockCondition.Error.BlockConditionFailed].")
     }
 
     func test__logger_receives_messages_from_produced_operation() {
 
         let produced = TestOperation(delay: 0)
         produced.name = "Produced Operation"
-        let operation = TestOperation(error: CloudContainerCondition.Error.NotAuthenticated, produced: produced)
+        let operation = TestOperation(error: BlockCondition.Error.BlockConditionFailed, produced: produced)
         operation.name = "Test Operation"
         addCompletionBlockToTestOperation(operation, withExpectation: expectationWithDescription("Test: \(__FUNCTION__)"))
 
@@ -71,7 +71,7 @@ class LoggingObserverTests: OperationTests {
         XCTAssertEqual(receivedMessages[1], "Test Operation: did produce operation: Produced Operation.")
         XCTAssertEqual(receivedMessages[2], "Produced Operation: did start.")
         XCTAssertEqual(receivedMessages[3], "Produced Operation: finished with no errors.")
-        XCTAssertEqual(receivedMessages[4], "Test Operation: finished with error(s): [Operations.CloudContainerCondition.Error.NotAuthenticated].")
+        XCTAssertEqual(receivedMessages[4], "Test Operation: finished with error(s): [Operations.BlockCondition.Error.BlockConditionFailed].")
     }
 }
 
