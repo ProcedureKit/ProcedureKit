8b2991556e18bf5c41430bfdc65f0a8f546dfc4e
Daniel Thorpe <danthorpe@me.com>
2015-10-06 21:26:37 +0100

    [OPR-100]: + docs to LoggingObserver

--- Operations/Core/Shared/LoggingObserver.swift
+++ Operations/Core/Shared/LoggingObserver.swift
@@ -10,15 +10,10 @@ import Foundation
 
 /**
 Attach a `LoggingObserver to an operation to log when the operation
-start, produces new operation and finsihed. It doesn't yet report
-detailed error infomation.
+start, produces new operation and finsihed.
 
 Any produced `Operation` instances will automatically get their
 own logger attached.
-    
-:param: queue, Provide a queue, by detault it uses it's own serial queue.
-:param: logger, Provide a logging block. By detault the logger uses `println`
-however, for custom loggers provide a block which receives a `String`.
 */
 public struct LoggingObserver: OperationObserver {
     public typealias LoggerBlockType = (message: String) -> Void
@@ -26,15 +21,47 @@ public struct LoggingObserver: OperationObserver {
     let logger: LoggerBlockType
     let queue: dispatch_queue_t
 
+    /**
+    Create a logging observer. Accepts as the final argument a block which receives a
+    `String` message to be logged. By default this just uses `print()`, but construct 
+    with a custom block to send logs to other systems. The block is executed on a 
+    dispatch queue.
+
+    - parameter queue: a queue, by detault it uses it's own serial queue.
+    - parameter logger: a logging block. By detault the logger uses `println`
+    however, for custom loggers provide a block which receives a `String`.
+    */
     public init(queue: dispatch_queue_t = Queue.Initiated.serial("me.danthorpe.Operations.Logger"), logger: LoggerBlockType = { print($0) }) {
         self.queue = queue
         self.logger = logger
     }
 
+    /**
+    Conforms to `OperationObserver`. The logger is sent a string which uses the
+    `name` parameter of the operation if provived.
+    
+       "My Operation: did start."
+    
+    - parameter operation: the `Operation` which has started.
+    */
     public func operationDidStart(operation: Operation) {
         log("\(operationName(operation)): did start.")
     }
 
+    /**
+    Conforms to `OperationObserver`. The logger is sent a string which uses the
+    `name` parameter of the operation if provived.
+
+        "My Operation: did produce operation: My Other Operation."
+
+    If the produced operation is an `Operation`, then a new `LoggingObserver` with 
+    same queue and logger will be attached to it as an observer. Meaning that when
+    the produced operation starts/produces/finishes, it will also generate log 
+    output.
+
+    - parameter operation: the `Operation` producer.
+    - parameter newOperation: the `Operation` which has been produced.
+    */
     public func operation(operation: Operation, didProduceOperation newOperation: NSOperation) {
         var detail = "\(newOperation)"
 
@@ -46,6 +73,20 @@ public struct LoggingObserver: OperationObserver {
         log("\(operationName(operation)): did produce operation: \(detail).")
     }
 
+    /**
+    Conforms to `OperationObserver`. The logger is sent a string which uses the
+    `name` parameter of the operation if provived. If there were errors, output
+    looks like
+
+        "My Operation: finsihed with error(s): [My Operation Error]."
+    
+    or if no errors:
+    
+        "My Operation: finsihed with no errors."
+
+    - parameter operation: the `Operation` that finished.
+    - parameter errors: an array of `ErrorType`, not that these will be printed out.
+    */
     public func operationDidFinish(operation: Operation, errors: [ErrorType]) {
         let detail = errors.count > 0 ? "error(s): \(errors)" : "no errors"
         log("\(operationName(operation)): finished with \(detail).")
