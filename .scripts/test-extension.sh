32fc9d51965649c5c7b01699516b125b20b9dd39
Daniel Thorpe <danthorpe@me.com>
2015-10-22 00:10:13 +0100

    [OPR-89]: Removes default lane.

--- .scripts/test-extension.sh
+++ .scripts/test-extension.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
-bundle update && bundle exec fastlane test_extension
+bundle update && bundle exec fastlane ios test_extension
