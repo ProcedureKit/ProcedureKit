5ab8afc1c15f322b8de057fe8af9fe6d7b231e8f
Daniel Thorpe <danthorpe@me.com>
2015-10-21 23:43:45 +0100

    [OPR-89]: Updates scripts.

--- .scripts/test-ios.sh
+++ .scripts/test-ios.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
-bundle update && bundle exec fastlane test
-bash <(curl -s https://codecov.io/bash) -D .fastlane/xcodebuild-data
+bundle update && bundle exec fastlane test_ios
