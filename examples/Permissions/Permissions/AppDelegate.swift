2689de8a6b1a7cdd80a92888c5030589bc6a4e53
Daniel Thorpe <danthorpe@me.com>
2015-11-10 14:52:35 +0000

    [OPR-108]: Tweaks to Persissions to help reveal interesting logging info.

--- examples/Permissions/Permissions/AppDelegate.swift
+++ examples/Permissions/Permissions/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        OperationLogger.globalLogLevelThreshold = .Notice
         return true
     }
 
