8be558d4cd47e261eb16b212c8e83705ef382516
Daniel Thorpe <danthorpe@me.com>
2015-12-17 15:51:00 +0000

    [OPR-144]: Updates the example project.

--- examples/Permissions/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ examples/Permissions/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,10 +86,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Operations.framework"
   install_framework "Pods/PureLayout.framework"
+  install_framework "Pods/SwiftyBeaver.framework"
   install_framework "Pods/TaylorSource.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Operations.framework"
   install_framework "Pods/PureLayout.framework"
+  install_framework "Pods/SwiftyBeaver.framework"
   install_framework "Pods/TaylorSource.framework"
 fi
