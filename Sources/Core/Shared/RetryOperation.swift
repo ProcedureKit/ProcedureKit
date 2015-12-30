080af0ce537aceacafa82d5bb20cd3e3e7613576
Daniel Thorpe <danthorpe@me.com>
2015-12-30 00:21:46 +0000

    [OPR-154]: Adds RetryOperation

--- Sources/Core/Shared/RetryOperation.swift
+++ Sources/Core/Shared/RetryOperation.swift
@@ -8,5 +8,13 @@
 
 import Foundation
 
+public class RetryOperation<O: NSOperation>: RepeatedOperation<AnyGenerator<O>> {
 
+    public init(min: NSTimeInterval = 1, max: MaximumTimeInterval = .Backoff, limit: Int? = .None, operation op: O) {
+        if let op = op as? Operation {
+            op.addCondition(NoFailedDependenciesCondition())
+        }
+        super.init(min: min, max: max, limit: limit, generator: anyGenerator { return op })
+    }
+}
 
