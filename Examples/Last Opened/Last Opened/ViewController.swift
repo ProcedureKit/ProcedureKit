6411c647f4ec8aa8c1272499bdcb9115d720b3ac
Daniel Thorpe <danthorpe@me.com>
2016-01-11 17:03:53 +0000

    [OPR-165]: Adds start of a CloudKit example app

---
+++ Examples/Last Opened/Last Opened/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Last Opened
+//
+//  Created by Daniel Thorpe on 11/01/2016.
+//  Copyright © 2016 Daniel Thorpe. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+import Operations
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
