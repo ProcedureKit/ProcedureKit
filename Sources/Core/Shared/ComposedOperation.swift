c5e985fc44cfbb9e7b844d0648f86f11910d64c0
Frank <difujia@gmail.com>
2016-03-22 18:24:34 +0800

    Fix retain cycle in composed operation.

--- Sources/Core/Shared/ComposedOperation.swift
+++ Sources/Core/Shared/ComposedOperation.swift
@@ -8,26 +8,28 @@
 
 import Foundation
 
-public class ComposedOperation<T: NSOperation>: Operation, OperationDidFinishObserver {
+public class ComposedOperation<T: NSOperation>: Operation {
 
     public let target: Operation
     public var operation: T
 
-    public convenience init(_ operation: T) {
-        self.init(operation: operation)
+    public convenience init(_ op: T) {
+        self.init(operation: op)
     }
 
-    init(operation composed: T) {
-        target = composed as? Operation ?? GroupOperation(operations: [composed])
-        operation = composed
+    init(operation op: T) {
+        self.target = op as? Operation ?? GroupOperation(operations: [op])
+        self.operation = op
         super.init()
         name = "Composed Operation"
-        target.name = "Composed <\(T.self)>"
-        target.addObserver(self)
+        target.name = target.name ?? "Composed <\(T.self)>"
+        target.addObserver(DidFinishObserver {
+            [unowned self] _, errors in
+            self.finish(errors)
+            })
     }
 
     public override func cancel() {
-        target.cancel()
         operation.cancel()
         super.cancel()
     }
@@ -35,11 +37,5 @@ public class ComposedOperation<T: NSOperation>: Operation, OperationDidFinishObs
     public override func execute() {
         target.log.severity = log.severity
         produceOperation(target)
-    }
-
-    public func didFinishOperation(operation: Operation, errors: [ErrorType]) {
-        if operation === target {
-            finish(errors)
-        }
     }
 }
