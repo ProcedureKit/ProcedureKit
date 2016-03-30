eabea5c7611526414f4321a30c419e63a95a47d5
Daniel Thorpe <danthorpe@users.noreply.github.com>
2016-03-30 10:15:27 +0100

    [OPR-256]: Composed Operation propagates canellation errors to target operation

--- Tests/Core/ComposedOperationTests.swift
+++ Tests/Core/ComposedOperationTests.swift
@@ -44,4 +44,39 @@ class ComposedOperationTests: OperationTests {
         XCTAssertTrue(composed.finished)
         XCTAssertTrue(composed.operation.didExecute)
     }
+    
+    func test__composed_operation_which_cancels_propagates_error_to_target() {
+        let target = TestOperation()
+        
+        var targetErrors: [ErrorType] = []
+        target.addObserver(CancelledObserver { op in
+            targetErrors = op.errors
+        })
+        
+        let operation = ComposedOperation(target)
+        
+        let operationError = TestOperation.Error.SimulatedError
+        operation.cancelWithError(operationError)
+
+        addCompletionBlockToTestOperation(operation)
+        runOperation(operation)
+        waitForExpectationsWithTimeout(3, handler: nil)
+        
+        XCTAssertEqual(targetErrors.count, 1)
+        
+        guard let error = targetErrors.first as? OperationError else {
+            XCTFail("Incorrect error received"); return
+        }
+        
+        switch error {
+        case let .ParentOperationCancelledWithErrors(parentErrors):
+            guard let parentError = parentErrors.first as? TestOperation.Error else {
+                XCTFail("Incorrect error received"); return
+            }
+            XCTAssertEqual(parentError, operationError)
+        default:
+            XCTFail("Incorrect error received"); return
+        }
+        
+    }
 }
