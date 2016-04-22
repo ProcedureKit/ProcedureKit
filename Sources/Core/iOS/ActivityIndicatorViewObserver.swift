0ae7cee10a6f0f527a0947b4746f90be40d9d81e
Matthew Holden <matthew.holden@mindbodyonline.com>
2016-04-22 11:43:09 -0700

    Implements ActivityIndicatorViewObserver
    (For earlier discussion, refer to #202.)
    
    `ActivityIndicatorViewObserver` is initialized with an
    `ActivityIndicatorViewAnimationInterface` type.
    
    The observer will call that activity indicator's `startAnimating()` and
    `stopAnimating()` in response to the observed operation's start and
    finish.

--- Sources/Core/iOS/ActivityIndicatorViewObserver.swift
+++ Sources/Core/iOS/ActivityIndicatorViewObserver.swift
@@ -2,14 +2,50 @@
 //  ActivityIndicatorViewObserver.swift
 //  Operations
 //
-//  Created by Daniel Thorpe on 16/04/2016.
+//  Created by Matthew Holden 16/04/2016.
 //
 //
 
-import Foundation
+import UIKit
 
-// TODO 🙂
+public protocol ActivityIndicatorViewAnimationInterface {
+    func startAnimating()
+    func stopAnimating()
+}
 
-public class ActivityIndicatorViewObserver {
+extension UIActivityIndicatorView: ActivityIndicatorViewAnimationInterface {}
 
+/**
+ An OperationObserverType that can update the state of an associated `UIActivityIndicatorView`
+ when the observed operation starts and finishes.
+ 
+ - note: Any type conforming to `ActivityIndicatorViewAnimationInterface` can be provided as 
+         the activity indicator.
+ */
+public class ActivityIndicatorViewObserver: OperationDidStartObserver, OperationDidFinishObserver {
+
+    private let activityIndicator: ActivityIndicatorViewAnimationInterface
+
+    /**
+     Initialize the observer with an `ActivityIndicatorViewAnimationInterface`-conforming type.
+     - parameter activityIndicator: the object to start and stop animating
+     - returns: an observer
+     - note: The activity indicator's `startAninating` and `stopAnimating` methods 
+             are guaranteed to execute on the main queue.
+     */
+    public init(activityIndicator: ActivityIndicatorViewAnimationInterface) {
+        self.activityIndicator = activityIndicator
+    }
+
+    public func didStartOperation(operation: Operation) {
+        dispatch_async(Queue.Main.queue) {
+            self.activityIndicator.startAnimating()
+        }
+    }
+
+    public func didFinishOperation(operation: Operation, errors: [ErrorType]) {
+        dispatch_async(Queue.Main.queue) {
+            self.activityIndicator.stopAnimating()
+        }
+    }
 }
--- Tests/Core/ActivityIndicatorViewObserverTests.swift
+++ Tests/Core/ActivityIndicatorViewObserverTests.swift
@@ -2,7 +2,7 @@
 //  ActivityIndicatorViewObserverTests.swift
 //  Operations
 //
-//  Created by Daniel Thorpe on 16/04/2016.
+//  Created by Matthew Holden 16/04/2016.
 //
 //
 
@@ -10,16 +10,44 @@ import Foundation
 import XCTest
 @testable import Operations
 
-// TODO 🙂
+enum AnimationState {
+    case Animating
+    case NotAnimating
+}
 
-class ActivityIndicatorViewObserverTests: XCTestCase {
+class TestableActivityIndicatorView: ActivityIndicatorViewAnimationInterface {
+    typealias IndicatorAnimationStateDidChangeBlock =  (toState: AnimationState) -> Void
+
+    let animationStateDidChange: IndicatorAnimationStateDidChangeBlock
+
+    init(_ didChange: IndicatorAnimationStateDidChangeBlock) {
+        animationStateDidChange = didChange
+    }
+
+    func startAnimating() {
+        animationStateDidChange(toState: .Animating)
+    }
+
+    func stopAnimating() {
+        animationStateDidChange(toState: .NotAnimating)
+    }
+}
+
+
+class ActivityIndicatorViewObserverTests: OperationTests {
 
     var operation: TestOperation!
-    var indicator: ActivityIndicatorViewObserver!
+    var indicator: ActivityIndicatorViewAnimationInterface!
+    var animationStateChanges: [AnimationState]!
 
     override func setUp() {
         super.setUp()
+        animationStateChanges = [AnimationState]()
         operation = TestOperation()
+        indicator = TestableActivityIndicatorView { [unowned self] animationState in
+            self.animationStateChanges.append(animationState)
+        }
+        operation.addObserver(ActivityIndicatorViewObserver(activityIndicator: indicator))
     }
 
     override func tearDown() {
@@ -29,10 +57,52 @@ class ActivityIndicatorViewObserverTests: XCTestCase {
     }
 
     func test__activity_indicator_starts_animating__when_operation_starts() {
+        addCompletionBlockToTestOperation(operation, withExpectation: expectationWithDescription("Test: \(#function)"))
 
+        var animationStateChangesBeforeWillFinish = 0
+        var firstAnimationChangeState: AnimationState? = .None
+
+        operation.addObserver(WillFinishObserver { [unowned self] _, _ in
+                animationStateChangesBeforeWillFinish = self.animationStateChanges.count
+                firstAnimationChangeState = self.animationStateChanges.first
+            })
+
+        runOperation(operation)
+
+        waitForExpectationsWithTimeout(3) { error in
+            XCTAssertTrue(self.operation.didExecute)
+            XCTAssertTrue(self.operation.finished)
+
+            XCTAssertEqual(animationStateChangesBeforeWillFinish, 1)
+            XCTAssertTrue(firstAnimationChangeState == .Animating)
+        }
     }
 
     func test__activity_indicator_stops_animating__when_operation_stops() {
+
+        let expectation = expectationWithDescription("Test: \(#function)")
+
+        operation.addCompletionBlock {
+            // ActivityIndicatorViewObserver will marshalls its invocation of the indicator's `stopAnimating` method
+            // onto the main queue. 
+            // As such, we need to ensure we don't fulfill this test's expectations
+            // until the run loop has had the opportunity to run one additional time.
+            dispatch_async(Queue.Main.queue) {
+                expectation.fulfill()
+            }
+        }
+
+        runOperation(operation)
+
+        waitForExpectationsWithTimeout(3) { [unowned self] error in
+            XCTAssertTrue(self.operation.didExecute)
+            XCTAssertTrue(self.operation.finished)
+
+            XCTAssertEqual(self.animationStateChanges.count, 2)
+
+            XCTAssertTrue(self.animationStateChanges.first == .Animating)
+            XCTAssertTrue(self.animationStateChanges.last == .NotAnimating)
+        }
 
     }
 }
