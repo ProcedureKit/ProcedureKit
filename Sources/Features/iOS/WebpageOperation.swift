d6b5266f35e7004f5ad2cb778ea5504823215c44
Andreas Braun <andreas.braun@bornholdtlee.de>
2016-05-13 18:25:30 +0200

    - Refactored WebpageOperation as ComposedOperation to fix a retain cycle. - Added Documentation.

--- Sources/Features/iOS/WebpageOperation.swift
+++ Sources/Features/iOS/WebpageOperation.swift
@@ -9,20 +9,35 @@
 import Foundation
 import SafariServices
 
-@available(iOS 9.0, *)
-public class WebpageOperation<From: PresentingViewController>: GroupOperation, SFSafariViewControllerDelegate {
 
-    var operation: UIOperation<SFSafariViewController, From>
+/**
+ An operation that presents an instance of the `SFSafariViewController` on a presenting view controller.
+ */
+@available(iOS 9.0, *)
+public class WebpageOperation<From: PresentingViewController>: ComposedOperation<UIOperation<SFSafariViewController, From>>, SFSafariViewControllerDelegate {
     
-    public init(url: NSURL, displayControllerFrom from: ViewControllerDisplayStyle<From>, entersReaderIfAvailable: Bool = true, sender: AnyObject? = .None) {
-        operation = UIOperation(controller: SFSafariViewController(URL: url, entersReaderIfAvailable: entersReaderIfAvailable), displayControllerFrom: from, sender: sender)
-        super.init(operations: [operation])
+    /**
+     Composes an operation that presents an instance of the `SFSafariViewController` on a presenting view controller.
+     
+     - parameter url: the `URL` that will be opend by `SFSafariViewController`.
+     - parameter displayControllerFrom: a `ViewControllerDisplayStyle`.
+     - parameter entersReaderIfAvailable: an optional flag that tells the `SFSafariViewController` to open the webpage in a reader mode if available.
+     - parameter sender: an `AnyObject` sender.
+     */
+    public convenience init(url: NSURL, displayControllerFrom from: ViewControllerDisplayStyle<From>, entersReaderIfAvailable: Bool = true, sender: AnyObject? = .None) {
+        let operation = UIOperation(controller: SFSafariViewController(URL: url, entersReaderIfAvailable: entersReaderIfAvailable), displayControllerFrom: from, sender: sender)
+        self.init(operation: operation)
         
         addObserver(WillExecuteObserver() { [weak self] _ in
             self?.operation.controller.delegate = self
         })
         
         addCondition(MutuallyExclusive<UIViewController>())
+    }
+    
+    // Annotated to be private so a consumer needs to call init(_:, displayControllerFrom:) because a URL is needed.
+    override private init(operation composed: UIOperation<SFSafariViewController, From>) {
+        super.init(operation: composed)
     }
 
     @objc public func safariViewControllerDidFinish(controller: SFSafariViewController) {
