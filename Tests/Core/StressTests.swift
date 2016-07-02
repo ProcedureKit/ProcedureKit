6b858957629ebf5a7ef106a35396dc1843557abf
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-07-01 20:04:45 -0400

    Add failing test
    test__SR_192_OperationQueue_delegate_weak_var_thread_safety:
    Tests race condition (and crash) involving OperationQueue’s weak
    delegate var.

--- Tests/Core/StressTests.swift
+++ Tests/Core/StressTests.swift
@@ -44,6 +44,46 @@ class StressTest: OperationTests {
         waitForOperation(operation)
         XCTAssertTrue(operation.didExecute)
     }
+    
+    func test__SR_192_OperationQueue_delegate_weak_var_thread_safety() {
+        //
+        // (SR-192): Weak properties are not thread safe when reading
+        // https://bugs.swift.org/browse/SR-192
+        //
+        // Affects: Swift < 3
+        // Fixed in: Swift 3.0+
+        // Without the code fix (marked with SR-192) in OperationQueue.swift, 
+        // this test will crash with EXC_BAD_ACCESS, or sometimes other errors.
+        //
+        class TestDelegate: OperationQueueDelegate {
+            func operationQueue(queue: OperationQueue, willAddOperation operation: NSOperation) { /* do nothing */ }
+            func operationQueue(queue: OperationQueue, willFinishOperation operation: NSOperation, withErrors errors: [ErrorType]) { /* do nothing */ }
+            func operationQueue(queue: OperationQueue, didFinishOperation operation: NSOperation, withErrors errors: [ErrorType]) { /* do nothing */ }
+        }
+        
+        let expectation = expectationWithDescription("Test: \(#function)")
+        var success = false
+        
+        dispatch_async(Queue.Initiated.queue) {
+            let group = dispatch_group_create()
+            for _ in 0..<1000000 {
+                let testQueue = OperationQueue()
+                testQueue.delegate = TestDelegate()
+                dispatch_group_async(group, dispatch_get_global_queue(0, 0), {
+                    let _ = testQueue.delegate
+                })
+                dispatch_group_async(group, dispatch_get_global_queue(0, 0), {
+                    let _ = testQueue.delegate
+                })
+            }
+            dispatch_group_wait(group,DISPATCH_TIME_FOREVER)
+            success = true
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(60, handler: nil)
+        XCTAssertTrue(success)
+    }
 
 }
 
