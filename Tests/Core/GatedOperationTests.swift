41e397ce8319d09d9dd9b3b4eba0507a14cf761f
Daniel Thorpe <danthorpe@me.com>
2016-07-25 23:15:01 +0100

    GatedOperation cancels composed operation when gate is closed (#377)
    * [OPR-377]: Adds early exit with guard
    
    * [OPR-377]: Refactors GatedOperation execute
    
    * [OPR-377]: Fixes typo

--- Tests/Core/GatedOperationTests.swift
+++ Tests/Core/GatedOperationTests.swift
@@ -11,23 +11,23 @@ import XCTest
 
 class GatedOperationTests: OperationTests {
 
-    func test__when_gate_is_closed_operation_is_not_performed() {
+    func test__when_gate_is_closed_operation_is_cancelled() {
+        let gate = GatedOperation(TestOperation()) { false }
 
-        let gate = GatedOperation(TestOperation()) { return false }
         addCompletionBlockToTestOperation(gate, withExpectation: expectationWithDescription("Test: \(#function)"))
-
         runOperation(gate)
 
         waitForExpectationsWithTimeout(5, handler: nil)
         XCTAssertTrue(gate.finished)
+        XCTAssertTrue(gate.operation.cancelled)
         XCTAssertFalse(gate.operation.didExecute)
     }
 
     func test__when_gate_is_open_operation_is_performed() {
-        let gate = GatedOperation(TestOperation()) { return true }
+        let gate = GatedOperation(TestOperation()) { true }
+
         addCompletionBlockToTestOperation(gate, withExpectation: expectationWithDescription("Test: \(#function), Gate"))
         addCompletionBlockToTestOperation(gate.operation, withExpectation: expectationWithDescription("Test: \(#function), Operation"))
-
         runOperation(gate)
 
         waitForExpectationsWithTimeout(5, handler: nil)
@@ -36,4 +36,70 @@ class GatedOperationTests: OperationTests {
         XCTAssertTrue(gate.operation.finished)
     }
 
+    func test__when_composed_is_dependency_and_gate_is_open__dependent_is_performed_after_composed() {
+        let operation = TestOperation()
+        let composed = TestOperation()
+        operation.addDependency(composed)
+        let gate = GatedOperation(composed) { true }
+
+        waitForOperations(operation, gate)
+
+        XCTAssertTrue(gate.finished)
+        XCTAssertTrue(composed.didExecute)
+        XCTAssertTrue(composed.finished)
+
+        XCTAssertTrue(operation.finished)
+        XCTAssertTrue(operation.didExecute)
+    }
+
+    func test__when_gate_is_dependency_and_gate_is_open__dependent_is_performed_after_gate() {
+        let operation = TestOperation()
+        let composed = TestOperation()
+        let gate = GatedOperation(composed) { true }
+        operation.addDependency(gate)
+
+        waitForOperations(operation, gate)
+
+        XCTAssertTrue(gate.finished)
+        XCTAssertTrue(composed.didExecute)
+        XCTAssertTrue(composed.finished)
+
+        XCTAssertTrue(operation.finished)
+        XCTAssertTrue(operation.didExecute)
+    }
+
+    func test__when_composed_is_dependency_and_gate_is_closed__dependent_is_still_performed() {
+        /// - Note - Remember that regardless of how a dependency finishes, dependent operations
+        /// will still become ready and execute.
+
+        let composed = TestOperation()
+        let operation = TestOperation()
+        operation.addDependency(composed)
+        let gate = GatedOperation(composed) { false }
+
+        waitForOperations(gate, operation)
+
+        XCTAssertTrue(gate.finished)
+        XCTAssertFalse(composed.didExecute)
+        XCTAssertTrue(composed.cancelled)
+
+        XCTAssertTrue(operation.finished)
+        XCTAssertTrue(operation.didExecute)
+    }
+
+    func test__when_gate_is_dependency_and_gate_is_closed__dependent_is_still_performed() {
+        let operation = TestOperation()
+        let composed = TestOperation()
+        let gate = GatedOperation(composed) { false }
+        operation.addDependency(gate)
+
+        waitForOperations(gate, operation)
+
+        XCTAssertTrue(gate.finished)
+        XCTAssertFalse(composed.didExecute)
+        XCTAssertTrue(composed.cancelled)
+
+        XCTAssertTrue(operation.finished)
+        XCTAssertTrue(operation.didExecute)
+    }
 }
