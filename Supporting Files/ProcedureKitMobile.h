dea75fc7ba8d8e025465e3f53b12695ea949a335
Daniel Thorpe <danthorpe@me.com>
2016-08-14 13:39:54 +0100

    [OPR-401]: Adds ProcedureKitMobile
    This will cover iOS & watchOS

---
+++ Supporting Files/ProcedureKitMobile.h
@@ -0,0 +1,17 @@
+//
+//  ProcedureKit
+//
+//  Copyright © 2016 ProcedureKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProcedureKit iOS.
+FOUNDATION_EXPORT double ProcedureKit_iOSVersionNumber;
+
+//! Project version string for ProcedureKit iOS.
+FOUNDATION_EXPORT const unsigned char ProcedureKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProcedureKit_iOS/PublicHeader.h>
+
+
