2271672a591f63b451441520113d65990cb48869
Daniel Thorpe <danthorpe@me.com>
2016-08-14 14:08:58 +0100

    [OPR-401]: Adds StressTests

---
+++ Tests/Stress Tests/ProcedureKitStressTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProcedureKit
+//
+//  Copyright © 2016 ProcedureKit. All rights reserved.
+//
+
+import XCTest
+import TestingProcedureKit
+@testable import ProcedureKit
+
+class TestSuiteRuns: XCTestCase {
+
+    func test__suite_runs() {
+        XCTAssertTrue(true)
+    }
+}
