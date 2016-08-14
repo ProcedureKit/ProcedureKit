fc78d2b108864fd56ceec511851eafcfed4b9d81
Daniel Thorpe <danthorpe@me.com>
2016-08-14 14:39:45 +0100

    [OPR-401]: Adds ProcedureKitCloud

--- Supporting Files/ProcedureKitMobile.h
+++ Supporting Files/ProcedureKitMobile.h
@@ -6,12 +6,12 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for ProcedureKit iOS.
-FOUNDATION_EXPORT double ProcedureKit_iOSVersionNumber;
+//! Project version number for ProcedureKitMobile.
+FOUNDATION_EXPORT double ProcedureKitMobileVersionNumber;
 
-//! Project version string for ProcedureKit iOS.
-FOUNDATION_EXPORT const unsigned char ProcedureKit_iOSVersionString[];
+//! Project version string for ProcedureKitMobile.
+FOUNDATION_EXPORT const unsigned char ProcedureKitMobileVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ProcedureKit_iOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <ProcedureKitMobile/PublicHeader.h>
 
 
