877446a609317062ec3a55e6605ced50b59035e7
Daniel Thorpe <danthorpe@me.com>
2016-08-28 00:20:35 +0100

    Adds Logging (#428)
    * [OPR-428]: Adds LogSeverity
    
    * [OPR-428]: Adds logger
    
    * [OPR-428]: Updates logging stuff
    
    * [OPR-428]: Adds logging tests

--- Sources/Operation+ProcedureKit.swift
+++ Sources/Operation+ProcedureKit.swift
@@ -8,6 +8,15 @@ import Foundation
 
 public extension Operation {
 
+    /**
+     Returns a non-optional `String` to use as the name
+     of an Operation. If the `name` property is not
+     set, this resorts to the class description.
+     */
+    var operationName: String {
+        return name ?? "Unnamed Operation"
+    }
+
     func addCompletionBlock(block: @escaping () -> Void) {
         if let existing = completionBlock {
             completionBlock = {
