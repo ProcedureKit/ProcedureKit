15486aa5831409bab05300fa6284d8a706ccb8a7
Daniel Thorpe <danthorpe@me.com>
2016-09-03 20:16:04 +0100

    Initial work on Result Injection (#446)

--- Sources/ProcedureProcotol.swift
+++ Sources/ProcedureProcotol.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-public protocol ProcedureProcotol {
+public protocol ProcedureProcotol: class {
 
     var isExecuting: Bool { get }
 
@@ -14,7 +14,7 @@ public protocol ProcedureProcotol {
 
     var isCancelled: Bool { get }
 
-//    var errors: [Error] { get }
+    var errors: [Error] { get }
 
     // Execution
 
@@ -44,6 +44,9 @@ public protocol ProcedureProcotol {
 
     func add<Observer: ProcedureObserver>(observer: Observer) where Observer.Procedure == Self
 
+    // Dependency
+
+    func add<Dependency: ProcedureProcotol>(dependency: Dependency)
 }
 
 public extension ProcedureProcotol {
@@ -64,4 +67,67 @@ public extension ProcedureProcotol {
 
     public func procedureDidFinish(withErrors: [Error]) { }
 
+
+    /**
+     Access the completed dependency operation before `self` is
+     started. This can be useful for transfering results/data between
+     operations.
+
+     - parameters dep: any `Operation` subclass.
+     - parameters block: a closure which receives `self`, the dependent
+     operation, and an array of `ErrorType`, and returns Void.
+     - returns: `self` - so that injections can be chained together.
+     */
+    func inject<Dependency: ProcedureProcotol>(dependency: Dependency, block: @escaping (Self, Dependency, [Error]) -> Void) -> Self {
+
+        dependency.addWillFinishBlockObserver { [weak self] dependency, errors in
+            if let strongSelf = self {
+                block(strongSelf, dependency, errors)
+            }
+        }
+
+        dependency.addDidCancelBlockObserver { [weak self] dependency, errors in
+            if let strongSelf = self {
+                strongSelf.cancel(withErrors: errors)
+            }
+        }
+
+        add(dependency: dependency)
+
+        return self
+    }
+}
+
+
+// MARK: - Result Injection
+
+public protocol ResultInjectionProtocol {
+
+    associatedtype Requirement
+    associatedtype Result
+
+    var requirement: Requirement { get set }
+    var result: Result { get }
+}
+
+public extension ResultInjectionProtocol {
+
+    public var requirement: Void {
+        get { return Void() }
+        set { }
+    }
+    public var result: Void { return Void() }
+}
+
+public extension ResultInjectionProtocol where Self: ProcedureProcotol {
+
+    func injectResultFrom<Dependency>(dependency: Dependency, block: @escaping (Self, Dependency, [Error]) -> Void) -> Self where Dependency: ProcedureProcotol, Dependency: ResultInjectionProtocol, Dependency.Result == Requirement {
+        return inject(dependency: dependency) { procedure, dependency, errors in
+            guard errors.isEmpty else {
+                procedure.cancel(withErrors: errors); return
+            }
+            var mutuableProcedure = procedure
+            mutuableProcedure.requirement = dependency.result
+        }
+    }
 }
