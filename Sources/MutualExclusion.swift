3a606081ff711ec7e3d217fb7f8a8126255b501c
Daniel Thorpe <danthorpe@me.com>
2016-09-13 11:05:24 +0100

    [OPR-448]: Adds MutualExclusion

---
+++ Sources/MutualExclusion.swift
@@ -0,0 +1,76 @@
+//
+//  ProcedureKit
+//
+//  Copyright © 2016 ProcedureKit. All rights reserved.
+//
+
+import Foundation
+
+internal class ExclusivityManager {
+
+    static let sharedInstance = ExclusivityManager()
+
+    fileprivate let queue = DispatchQueue.initiated
+    fileprivate var procedures: [String: [Procedure]] = [:]
+
+    private init() {
+        // A private initalizer prevents any other part of the app
+        // from creating an instance.
+    }
+
+    func add(procedure: Procedure, category: String) -> Operation? {
+        return queue.sync { self._add(procedure: procedure, category: category) }
+    }
+
+    func remove(procedure: Procedure, category: String) {
+        queue.async { self._remove(procedure: procedure, category: category) }
+    }
+
+    fileprivate func _add(procedure: Procedure, category: String) -> Operation? {
+        procedure.log.verbose(message: ">>> \(category)")
+
+        procedure.addDidFinishBlockObserver { [unowned self] (procedure, errors) in
+            self.remove(procedure: procedure, category: category)
+        }
+
+        var proceduresWithThisCategory = procedures[category] ?? []
+
+        let previous = proceduresWithThisCategory.last
+
+        if let previous = previous {
+            procedure.add(dependencyOnPreviousMutuallyExclusiveProcedure: previous)
+        }
+
+        proceduresWithThisCategory.append(procedure)
+
+        procedures[category] = proceduresWithThisCategory
+
+        return previous
+    }
+
+    fileprivate func _remove(procedure: Procedure, category: String) {
+        procedure.log.verbose(message: "<<< \(category)")
+
+        if let proceduresWithThisCategory = procedures[category], let index = proceduresWithThisCategory.index(of: procedure) {
+            var mutableProceduresWithThisCategory = proceduresWithThisCategory
+            mutableProceduresWithThisCategory.remove(at: index)
+            procedures[category] = mutableProceduresWithThisCategory
+        }
+    }
+}
+
+internal extension ExclusivityManager {
+
+    /// This should only be used as part of the unit testing
+    /// and in v2+ will not be publically accessible
+    func __tearDownForUnitTesting() {
+        queue.sync {
+            for (category, procedures) in self.procedures {
+                for procedure in procedures {
+                    procedure.cancel()
+                    self._remove(procedure: procedure, category: category)
+                }
+            }
+        }
+    }
+}
