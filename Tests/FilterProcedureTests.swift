1f2a8582418e74a1e6a15a77b3e9c6f7a492c9c9
Daniel Thorpe <danthorpe@me.com>
2016-10-08 21:07:43 +0100

    [OPR-496]: Adds filter method for when Result is a sequence

--- Tests/FilterProcedureTests.swift
+++ Tests/FilterProcedureTests.swift
@@ -8,6 +8,28 @@ import XCTest
 import TestingProcedureKit
 @testable import ProcedureKit
 
+class NumbersProcedure: Procedure, ResultInjectionProtocol {
+
+    var requirement: Void = ()
+    var result: Array<Int> = []
+    var error: Error? = nil
+
+    init(error: Error? = nil) {
+        self.error = error
+        super.init()
+    }
+
+    override func execute() {
+        if let error = error {
+            finish(withError: error)
+        }
+        else {
+            result = [0, 1, 2, 3, 4, 5 , 6 , 7, 8, 9]
+            finish()
+        }
+    }
+}
+
 class FilterProcedureTests: ProcedureKitTestCase {
 
     func test__requirement_is_filtered_to_result() {
@@ -21,5 +43,22 @@ class FilterProcedureTests: ProcedureKitTestCase {
         let evenOnly = FilterProcedure(source: [0,1,2,3,4,5,6,7]) { _ in throw TestError() }
         wait(for: evenOnly)
         XCTAssertProcedureFinishedWithErrors(evenOnly, count: 1)
+    }
+
+    func test__filter_dependency_which_finishes_without_errors() {
+        let numbers = NumbersProcedure()
+        let filtered = numbers.filter { $0 % 2 == 0 }
+        wait(for: numbers, filtered)
+        XCTAssertProcedureFinishedWithoutErrors(numbers)
+        XCTAssertProcedureFinishedWithoutErrors(filtered)
+        XCTAssertEqual(filtered.result, [0,2,4,6,8])
+    }
+
+    func test__filter_dependency_which_finishes_with_errors() {
+        let numbers = NumbersProcedure(error: TestError())
+        let filtered = numbers.filter { $0 % 2 == 0 }
+        wait(for: numbers, filtered)
+        XCTAssertProcedureFinishedWithErrors(numbers, count: 1)
+        XCTAssertProcedureCancelledWithErrors(filtered, count: 1)
     }
 }
