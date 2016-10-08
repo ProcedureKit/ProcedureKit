13b06f8f794e262c4305b01983b6f5628c822766
Daniel Thorpe <danthorpe@me.com>
2016-10-08 22:27:38 +0100

    Adds ReduceProcedure (#497)
    * [OPR-497]: Adds ReduceProcedure
    
    * [OPR-497]: Refactors Map, Filter adds FlatMap
    
    MapProcedure is really a TransformProcedure. MapProcedure and FilterProcedure are now simple subclasses of ReduceProcedure, along with FlatMapProcedure.

--- Sources/Filter.swift
+++ Sources/Filter.swift
@@ -6,42 +6,23 @@
 
 import Foundation
 
-public class FilterProcedure<Element>: Procedure, ResultInjectionProtocol {
+public class FilterProcedure<Element>: ReduceProcedure<Element, Array<Element>> {
 
-    public var requirement: AnySequence<Element>
-    public var result: Array<Element> = []
-    public let isIncluded: (Element) throws -> Bool
-
-    public init<S: Sequence>(source: S, isIncluded block: @escaping (Element) throws -> Bool) where S.Iterator.Element == Element, S.SubSequence: Sequence, S.SubSequence.Iterator.Element == Element, S.SubSequence.SubSequence == S.SubSequence {
-        requirement = AnySequence(source)
-        isIncluded = block
-        super.init()
+    public init<S: Sequence>(source: S, isIncluded: @escaping (Element) throws -> Bool) where S.Iterator.Element == Element, S.SubSequence: Sequence, S.SubSequence.Iterator.Element == Element, S.SubSequence.SubSequence == S.SubSequence {
+        super.init(source: source, initial: []) { acc, element in
+            guard try isIncluded(element) else { return acc }
+            return acc + [element]
+        }
     }
 
     public convenience init(isIncluded block: @escaping (Element) throws -> Bool) {
         self.init(source: [], isIncluded: block)
-    }
-
-    public override func execute() {
-        var finishingError: Error? = nil
-        defer { finish(withError: finishingError) }
-        do {
-            result = try requirement.filter(isIncluded)
-        }
-        catch { finishingError = error }
     }
 }
 
 public extension ProcedureProtocol where Self: ResultInjectionProtocol, Self.Result: Sequence {
 
     func filter(includeElement: @escaping (Result.Iterator.Element) throws -> Bool) -> FilterProcedure<Result.Iterator.Element> {
-        let filter = FilterProcedure(isIncluded: includeElement)
-        filter.inject(dependency: self) { filter, dependency, errors in
-            guard errors.isEmpty else {
-                filter.cancel(withError: ProcedureKitError.dependency(finishedWithErrors: errors)); return
-            }
-            filter.requirement = AnySequence(Array(dependency.result))
-        }
-        return filter
+        return injectRequirement(FilterProcedure(isIncluded: includeElement))
     }
 }
---
+++ Sources/Reduce.swift
@@ -0,0 +1,56 @@
+//
+//  ProcedureKit
+//
+//  Copyright © 2016 ProcedureKit. All rights reserved.
+//
+
+import Foundation
+
+public class ReduceProcedure<Element, U>: Procedure, ResultInjectionProtocol {
+
+    public var requirement: AnySequence<Element>
+    public var result: U
+    public let initial: U
+    public let nextPartialResult: (U, Element) throws -> U
+
+    public init<S: Sequence>(source: S, initial: U, nextPartialResult block: @escaping (U, Element) throws -> U) where S.Iterator.Element == Element, S.SubSequence: Sequence, S.SubSequence.Iterator.Element == Element, S.SubSequence.SubSequence == S.SubSequence {
+        self.requirement = AnySequence(source)
+        self.initial = initial
+        self.result = initial
+        self.nextPartialResult = block
+        super.init()
+    }
+
+    public convenience init(initial: U, nextPartialResult block: @escaping (U, Element) throws -> U) {
+        self.init(source: [], initial: initial, nextPartialResult: block)
+    }
+
+    public override func execute() {
+        var finishingError: Error? = nil
+        defer { finish(withError: finishingError) }
+        do {
+            result = try requirement.reduce(initial, nextPartialResult)
+        }
+        catch { finishingError = error }
+    }
+}
+
+internal extension ProcedureProtocol where Self: ResultInjectionProtocol, Self.Result: Sequence {
+
+    func injectRequirement<P: ProcedureProtocol>(_ procedure: P) -> P where P: ResultInjectionProtocol, P.Requirement == AnySequence<Self.Result.Iterator.Element> {
+        procedure.inject(dependency: self) { procedure, dependency, errors in
+            guard errors.isEmpty else {
+                procedure.cancel(withError: ProcedureKitError.dependency(finishedWithErrors: errors)); return
+            }
+            procedure.requirement = AnySequence(Array(dependency.result))
+        }
+        return procedure
+    }
+}
+
+public extension ProcedureProtocol where Self: ResultInjectionProtocol, Self.Result: Sequence {
+
+    func reduce<U>(_ initial: U, nextPartialResult: @escaping (U, Result.Iterator.Element) throws -> U) -> ReduceProcedure<Result.Iterator.Element, U> {
+        return injectRequirement(ReduceProcedure(initial: initial, nextPartialResult: nextPartialResult))
+    }
+}
