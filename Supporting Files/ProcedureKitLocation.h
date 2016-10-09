259264e4c84d74f6ecbb717dd468c76c8c86fc84
Daniel Thorpe <danthorpe@me.com>
2016-10-09 17:23:10 +0100

    [OPR-499]: Adds ProcedureKitLocation framework

---
+++ Supporting Files/ProcedureKitLocation.h
@@ -0,0 +1,18 @@
+//
+//  ProcedureKit
+//
+//  Copyright © 2016 ProcedureKit. All rights reserved.
+//
+
+@import Foundation;
+@import CoreLocation;
+
+//! Project version number for ProcedureKitLocation.
+FOUNDATION_EXPORT double ProcedureKitLocationVersionNumber;
+
+//! Project version string for ProcedureKitLocation.
+FOUNDATION_EXPORT const unsigned char ProcedureKitLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProcedureKitLocation/PublicHeader.h>
+
+
