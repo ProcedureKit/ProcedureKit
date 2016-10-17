1eafa783e5e0b5d2d5d3992e10f0c1ca123b193c
Daniel Thorpe <danthorpe@me.com>
2016-10-17 12:50:48 +0100

    [OPR-524]: Adds ProcedureKitNetwork framework

---
+++ Supporting Files/ProcedureKitNetwork.h
@@ -0,0 +1,19 @@
+//
+//  ProcedureKitNetwork.h
+//  ProcedureKitNetwork
+//
+//  Created by Daniel Thorpe on 17/10/2016.
+//  Copyright © 2016 ProcedureKit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProcedureKitNetwork.
+FOUNDATION_EXPORT double ProcedureKitNetworkVersionNumber;
+
+//! Project version string for ProcedureKitNetwork.
+FOUNDATION_EXPORT const unsigned char ProcedureKitNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProcedureKitNetwork/PublicHeader.h>
+
+
