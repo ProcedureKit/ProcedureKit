db2a73da75acb4a8e2c1338b41919634f9e0fefd
Daniel Thorpe <danthorpe@me.com>
2016-10-17 13:04:27 +0100

    Adds ProcedureKitNetwork framework (#524)
    * [OPR-524]: Adds ProcedureKitNetwork framework
    
    * [OPR-524]: Adds Network to CI jobs
    
    * [OPR-524]: Renames Stress Tests job
    
    * [OPR-524]: Shares the network scheme
    
    * [OPR-524]: Imports Foundation instead of Cocoa
    
    * [OPR-524]: Forces Stress Tests onto a non-simulator agent

---
+++ Supporting Files/ProcedureKitNetwork.h
@@ -0,0 +1,17 @@
+//
+//  ProcedureKit
+//
+//  Copyright © 2016 ProcedureKit. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for ProcedureKitNetwork.
+FOUNDATION_EXPORT double ProcedureKitNetworkVersionNumber;
+
+//! Project version string for ProcedureKitNetwork.
+FOUNDATION_EXPORT const unsigned char ProcedureKitNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProcedureKitNetwork/PublicHeader.h>
+
+
