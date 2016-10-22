bf940ee2db25d89a8b52a71a0728df4b0d4fe1f5
Daniel Thorpe <danthorpe@me.com>
2016-10-23 00:02:08 +0100

    [OPR-537]: Fixes simple unit tests

--- Tests/Network/ResilientNetworkProcedureTests.swift
+++ Tests/Network/ResilientNetworkProcedureTests.swift
@@ -15,10 +15,10 @@ struct Behavior: ResilientNetworkBehavior {
     var errorDelay: Delay? = nil
     var subsequentAttemptDelay: Delay = .by(0.1)
 
-    var retryRequestForResponseWithStatusCode: (Int) -> Bool = { _ in true }
+    var retryRequestForResponseWithStatusCode: (Int, Int?) -> Bool = { _, _ in false }
 
-    func retryRequest(forResponseWithStatusCode statusCode: Int) -> Bool {
-        return retryRequestForResponseWithStatusCode(statusCode)
+    public func retryRequest(forResponseWithStatusCode statusCode: Int, errorCode: Int?) -> Bool {
+        return retryRequestForResponseWithStatusCode(statusCode, errorCode)
     }
 }
 
@@ -35,25 +35,11 @@ class ResilientNetworkProcedureTests: ProcedureKitTestCase {
         download = ResilientNetworkProcedure(behavior: behavior) { NetworkDataProcedure(session: self.session, request: URLRequest(url: "http://procedure.kit.run")) }
     }
 
-    func test__for_reals() {
-        download = ResilientNetworkProcedure(behavior: behavior) { NetworkDataProcedure(session: URLSession.shared, request: URLRequest(url: "http://procedure.kit.run")) }
-        wait(for: download)
-        XCTAssertProcedureFinishedWithoutErrors(download)
-    }
-
     func test__given__valid_response__then_one_request_made() {
         wait(for: download)
         XCTAssertProcedureFinishedWithoutErrors(download)
         XCTAssertEqual(download.count, 1)
         XCTAssertEqual(download.data, session.returnedData)
     }
-
-    func test__given__error_response__then_one_request_made() {
-        session.returnedResponse = HTTPURLResponse(url: "http://procedure.kit.run", statusCode: 500, httpVersion: nil, headerFields: nil)
-        wait(for: download)
-        XCTAssertProcedureFinishedWithErrors(download, count: 1)
-        XCTAssertEqual(download.count, 1)
-    }
-
 }
 
