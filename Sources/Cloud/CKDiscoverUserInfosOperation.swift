8f200c083b0acdede866615552d5a348a956ae21
Daniel Thorpe <danthorpe@me.com>
2016-10-23 15:48:25 +0100

    [OPR-542]: Adds extensions for CKDiscoverUserInfosOperation

--- Sources/Cloud/CKDiscoverUserInfosOperation.swift
+++ Sources/Cloud/CKDiscoverUserInfosOperation.swift
@@ -6,6 +6,12 @@
 
 import CloudKit
 
+public struct DiscoverUserInfosError<RecordID: Hashable, DiscoveredUserInfo>: CloudKitError {
+    public let underlyingError: Error
+    public let userInfoByEmail: [String: DiscoveredUserInfo]?
+    public let userInfoByRecordID: [RecordID: DiscoveredUserInfo]?
+}
+
 @available(iOS, introduced: 8.0, deprecated: 10.0, message: "Use CKDiscoverUserIdentitiesOperation instead")
 @available(OSX, introduced: 10.10, deprecated: 10.12, message: "Use CKDiscoverUserIdentitiesOperation instead")
 @available(tvOS, introduced: 8.0, deprecated: 10.0, message: "Use CKDiscoverUserIdentitiesOperation instead")
@@ -13,5 +19,64 @@ import CloudKit
 extension CKDiscoverUserInfosOperation: CKDiscoverUserInfosOperationProtocol, AssociatedErrorProtocol {
 
     // The associated error type
-    public typealias AssociatedError = PKCKError
+    public typealias AssociatedError = DiscoverUserInfosError<RecordID, DiscoveredUserInfo>
+}
+
+extension CKProcedure where T: CKDiscoverUserInfosOperationProtocol, T: AssociatedErrorProtocol, T.AssociatedError: CloudKitError {
+
+    public var emailAddresses: [String]? {
+        get { return operation.emailAddresses }
+        set { operation.emailAddresses = newValue }
+    }
+
+    public var userRecordIDs: [T.RecordID]? {
+        get { return operation.userRecordIDs }
+        set { operation.userRecordIDs = newValue }
+    }
+
+    func setDiscoverUserInfosCompletionBlock(_ block: @escaping CloudKitProcedure<T>.DiscoverUserInfosCompletionBlock) {
+        operation.discoverUserInfosCompletionBlock = { [weak self] userInfoByEmail, userInfoByRecordID, error in
+            if let strongSelf = self, let error = error {
+                strongSelf.append(fatalError: DiscoverUserInfosError(underlyingError: error, userInfoByEmail: userInfoByEmail, userInfoByRecordID: userInfoByRecordID))
+            }
+            else {
+                block(userInfoByEmail, userInfoByRecordID)
+            }
+        }
+    }
+}
+
+extension CloudKitProcedure where T: CKDiscoverUserInfosOperationProtocol, T: AssociatedErrorProtocol, T.AssociatedError: CloudKitError {
+
+    /// A typealias for the block type used by CloudKitOperation<CKDiscoverUserInfosOperation>
+    public typealias DiscoverUserInfosCompletionBlock = ([String: T.DiscoveredUserInfo]?, [T.RecordID: T.DiscoveredUserInfo]?) -> Void
+
+    /// - returns: get or set the email addresses
+    public var emailAddresses: [String]? {
+        get { return current.emailAddresses }
+        set {
+            current.emailAddresses = newValue
+            appendConfigureBlock { $0.emailAddresses = newValue }
+        }
+    }
+
+    /// - returns: get or set the user records IDs
+    public var userRecordIDs: [T.RecordID]? {
+        get { return current.userRecordIDs }
+        set {
+            current.userRecordIDs = newValue
+            appendConfigureBlock { $0.userRecordIDs = newValue }
+        }
+    }
+
+    /**
+     Before adding the CloudKitOperation instance to a queue, set a completion block
+     to collect the results in the successful case. Setting this completion block also
+     ensures that error handling gets triggered.
+
+     - parameter block: a DiscoverUserInfosCompletionBlock block
+     */
+    public func setDiscoverUserInfosCompletionBlock(block: @escaping DiscoverUserInfosCompletionBlock) {
+        appendConfigureBlock { $0.setDiscoverUserInfosCompletionBlock(block) }
+    }
 }
