c9b68057d6a110b04dcc173668ad09cd8afafcbf
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 18:32:42 -0400

    [OPR-542]: Adds tests for CKFetchRecordZoneChangesOperation

--- Tests/Cloud/CKFetchRecordZoneChangesOperationTests.swift
+++ Tests/Cloud/CKFetchRecordZoneChangesOperationTests.swift
@@ -41,3 +41,103 @@ class TestCKFetchRecordZoneChangesOperation: TestCKDatabaseOperation, CKFetchRec
         fetchRecordZoneChangesCompletionBlock?(outputError)
     }
 }
+
+class CKFetchRecordZoneChangesOperationTests: CKProcedureTestCase {
+
+    var target: TestCKFetchRecordZoneChangesOperation!
+    var operation: CKProcedure<TestCKFetchRecordZoneChangesOperation>!
+
+    override func setUp() {
+        super.setUp()
+        target = TestCKFetchRecordZoneChangesOperation()
+        operation = CKProcedure(operation: target)
+    }
+
+    func test__set_get__recordZoneIDs() {
+        let recordZoneIDs: [String] = ["I'm a record zone ID"]
+        operation.recordZoneIDs = recordZoneIDs
+        XCTAssertEqual(operation.recordZoneIDs, recordZoneIDs)
+        XCTAssertEqual(target.recordZoneIDs, recordZoneIDs)
+    }
+
+    func test__set_get__optionsByRecordZoneID() {
+        let optionsByRecordZoneID = ["zone-id": "testoption"]
+        operation.optionsByRecordZoneID = optionsByRecordZoneID
+        XCTAssertNotNil(operation.optionsByRecordZoneID)
+        XCTAssertEqual(operation.optionsByRecordZoneID!, optionsByRecordZoneID)
+        XCTAssertNotNil(target.optionsByRecordZoneID)
+        XCTAssertEqual(target.optionsByRecordZoneID!, optionsByRecordZoneID)
+    }
+
+    func test__set_get__recordChangedBlock() {
+        var setByBlock = false
+        let block: (String) -> Void = { record in
+            setByBlock = true
+        }
+        operation.recordChangedBlock = block
+        XCTAssertNotNil(operation.recordChangedBlock)
+        target.recordChangedBlock?("I'm a record")
+        XCTAssertTrue(setByBlock)
+    }
+
+    func test__set_get__recordWithIDWasDeletedBlock() {
+        var setByBlock = false
+        let block: (String, String) -> Void = { recordID, recordType in
+            setByBlock = true
+        }
+        operation.recordWithIDWasDeletedBlock = block
+        XCTAssertNotNil(operation.recordWithIDWasDeletedBlock)
+        target.recordWithIDWasDeletedBlock?("I'm a record ID", "ExampleType")
+        XCTAssertTrue(setByBlock)
+    }
+
+    func test__set_get__recordZoneChangeTokensUpdatedBlock() {
+        var setByBlock = false
+        let block: (String, String?, Data?) -> Void = { recordZoneID, serverChangeToken, clientChangeTokenData in
+            setByBlock = true
+        }
+        operation.recordZoneChangeTokensUpdatedBlock = block
+        XCTAssertNotNil(operation.recordZoneChangeTokensUpdatedBlock)
+        target.recordZoneChangeTokensUpdatedBlock?("I'm a record ID", "I'm a server change token", nil)
+        XCTAssertTrue(setByBlock)
+    }
+
+    func test__set_get__recordZoneFetchCompletionBlock() {
+        var setByBlock = false
+        let block: (String, String?, Data?, Bool, Error?) -> Void = { recordZoneID, serverChangeToken, clientChangeTokenData, moreComing, recordZoneError in
+            setByBlock = true
+        }
+        operation.recordZoneFetchCompletionBlock = block
+        XCTAssertNotNil(operation.recordZoneFetchCompletionBlock)
+        target.recordZoneFetchCompletionBlock?("I'm a record ID", "I'm a server change token", nil, false, nil)
+        XCTAssertTrue(setByBlock)
+    }
+
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setFetchRecordZoneChangesCompletionBlock { didExecuteBlock = true }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block() {
+        target.setSimulationOutputError(error: TestError())
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setFetchRecordZoneChangesCompletionBlock { didExecuteBlock = true }
+        target.setSimulationOutputError(error: TestError())
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
