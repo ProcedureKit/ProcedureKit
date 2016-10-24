c6137cfb696fc85766bb446619cae404296f38f9
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 12:04:28 -0400

    Adds tests for CKDiscoverAllContactsOperation

--- Tests/Cloud/CKDiscoverAllContactsOperationTests.swift
+++ Tests/Cloud/CKDiscoverAllContactsOperationTests.swift
@@ -28,5 +28,49 @@ class TestCKDiscoverAllContactsOperation: TestCKOperation, CKDiscoverAllContacts
         discoverAllContactsCompletionBlock?(result, error)
     }
 }
+    
+class CKDiscoverAllContactsOperationTests: CKProcedureTestCase {
+
+    var target: TestCKDiscoverAllContactsOperation!
+    var operation: CKProcedure<TestCKDiscoverAllContactsOperation>!
+
+    override func setUp() {
+        super.setUp()
+        target = TestCKDiscoverAllContactsOperation()
+        operation = CKProcedure(operation: target)
+    }
+
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setDiscoverAllContactsCompletionBlock { _ in
+            didExecuteBlock = true
+        }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block() {
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setDiscoverAllContactsCompletionBlock { _ in
+            didExecuteBlock = true
+        }
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
 
 #endif
