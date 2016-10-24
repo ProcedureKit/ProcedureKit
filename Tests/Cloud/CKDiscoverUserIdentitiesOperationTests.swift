0d01f4b3d35c5dad57d445b21b84c5e07387d351
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 12:05:23 -0400

    Adds tests for CKDiscoverUserIdentitiesOperation

--- Tests/Cloud/CKDiscoverUserIdentitiesOperationTests.swift
+++ Tests/Cloud/CKDiscoverUserIdentitiesOperationTests.swift
@@ -28,4 +28,60 @@ class TestCKDiscoverUserIdentitiesOperation: TestCKOperation, CKDiscoverUserIden
     }
 }
 
+class CKDiscoverUserIdentitiesOperationTests: CKProcedureTestCase {
 
+    var target: TestCKDiscoverUserIdentitiesOperation!
+    var operation: CKProcedure<TestCKDiscoverUserIdentitiesOperation>!
+
+    override func setUp() {
+        super.setUp()
+        target = TestCKDiscoverUserIdentitiesOperation()
+        operation = CKProcedure(operation: target)
+    }
+
+    func test__set_get__userIdentityLookupInfos() {
+        let userIdentityLookupInfos = [ "hello@world.com" ]
+        operation.userIdentityLookupInfos = userIdentityLookupInfos
+        XCTAssertEqual(operation.userIdentityLookupInfos, userIdentityLookupInfos)
+        XCTAssertEqual(target.userIdentityLookupInfos, userIdentityLookupInfos)
+    }
+
+    func test__set_get__userIdentityDiscoveredBlock() {
+        var setByCompletionBlock = false
+        let block: (String, String) -> Void = { identity, lookupInfo in
+            setByCompletionBlock = true
+        }
+        operation.userIdentityDiscoveredBlock = block
+        XCTAssertNotNil(operation.userIdentityDiscoveredBlock)
+        target.userIdentityDiscoveredBlock?("Example", "hello@world.com")
+        XCTAssertTrue(setByCompletionBlock)
+    }
+
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setDiscoverUserIdentitiesCompletionBlock { didExecuteBlock = true }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block() {
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setDiscoverUserIdentitiesCompletionBlock { didExecuteBlock = true }
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
