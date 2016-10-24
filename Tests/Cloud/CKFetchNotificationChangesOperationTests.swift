fdf803f7d4f1ddc44ce660fc63e47cd5d3216274
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 16:00:10 -0400

    [OPR-542]: Adds tests for CKFetchNotificationChangesOperation

--- Tests/Cloud/CKFetchNotificationChangesOperationTests.swift
+++ Tests/Cloud/CKFetchNotificationChangesOperationTests.swift
@@ -37,3 +37,79 @@ class TestCKFetchNotificationChangesOperation: TestCKOperation, CKFetchNotificat
         fetchNotificationChangesCompletionBlock?(finalPreviousServerChangeToken, error)
     }
 }
+
+class CKFetchNotificationChangesOperationTests: CKProcedureTestCase {
+
+    var target: TestCKFetchNotificationChangesOperation!
+    var operation: CKProcedure<TestCKFetchNotificationChangesOperation>!
+
+    override func setUp() {
+        super.setUp()
+        target = TestCKFetchNotificationChangesOperation()
+        operation = CKProcedure(operation: target)
+    }
+
+    func test__set_get__previousServerChangeToken() {
+        let previousServerChangeToken: String = "I'm a server token"
+        operation.previousServerChangeToken = previousServerChangeToken
+        XCTAssertEqual(operation.previousServerChangeToken, previousServerChangeToken)
+        XCTAssertEqual(target.previousServerChangeToken, previousServerChangeToken)
+    }
+    
+    func test__set_get__resultsLimits() {
+        let limit: Int = 100
+        operation.resultsLimit = limit
+        XCTAssertEqual(operation.resultsLimit, limit)
+        XCTAssertEqual(target.resultsLimit, limit)
+    }
+
+    func test__get__moreComing() {
+        target.moreComing = true
+        XCTAssertTrue(operation.moreComing)
+        target.moreComing = false
+        XCTAssertFalse(operation.moreComing)
+    }
+
+    func test__set_get__notificationChangedBlock() {
+        var setByBlock = false
+        let block: (String) -> Void = { notification in
+            setByBlock = true
+        }
+        operation.notificationChangedBlock = block
+        XCTAssertNotNil(operation.notificationChangedBlock)
+        target.notificationChangedBlock?("notification")
+        XCTAssertTrue(setByBlock)
+    }
+
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setFetchNotificationChangesCompletionBlock { _ in
+            didExecuteBlock = true
+        }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block() {
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setFetchNotificationChangesCompletionBlock { _ in
+            didExecuteBlock = true
+        }
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
