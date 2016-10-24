a3203b1d0e1d389dc9eed7162399bd3e92f0a8e4
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 12:04:42 -0400

    Adds tests for CKDiscoverAllUserIdentitiesOperation

--- Tests/Cloud/CKDiscoverAllUserIdentitiesOperationTests.swift
+++ Tests/Cloud/CKDiscoverAllUserIdentitiesOperationTests.swift
@@ -29,4 +29,55 @@ class TestCKDiscoverAllUserIdentitiesOperation: TestCKOperation, CKDiscoverAllUs
     }
 }
 
+class CKDiscoverAllUserIdentitiesOperationTests: CKProcedureTestCase {
+
+    var target: TestCKDiscoverAllUserIdentitiesOperation!
+    var operation: CKProcedure<TestCKDiscoverAllUserIdentitiesOperation>!
+
+    override func setUp() {
+        super.setUp()
+        target = TestCKDiscoverAllUserIdentitiesOperation()
+        operation = CKProcedure(operation: target)
+    }
+
+    func test__set_get__userIdentityDiscoveredBlock() {
+        var setByCompletionBlock = false
+        let block: (String) -> Void = { identity in
+            setByCompletionBlock = true
+        }
+        operation.userIdentityDiscoveredBlock = block
+        XCTAssertNotNil(operation.userIdentityDiscoveredBlock)
+        target.userIdentityDiscoveredBlock?("hello@world.com")
+        XCTAssertTrue(setByCompletionBlock)
+    }
+
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setDiscoverAllUserIdentitiesCompletionBlock { didExecuteBlock = true }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block() {
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setDiscoverAllUserIdentitiesCompletionBlock { didExecuteBlock = true }
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
+
 #endif
