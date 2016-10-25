9ff346fb7e257b44ca8eb78109e73b09b93a8ae4
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 20:56:12 -0400

    [OPR-542]: Adds tests for CKModifyRecordZonesOperation

--- Tests/Cloud/CKModifyRecordZonesOperationTests.swift
+++ Tests/Cloud/CKModifyRecordZonesOperationTests.swift
@@ -32,3 +32,65 @@ class TestCKModifyRecordZonesOperation: TestCKDatabaseOperation, CKModifyRecordZ
     }
 }
 
+class CKModifyRecordZonesOperationTests: CKProcedureTestCase {
+
+    var target: TestCKModifyRecordZonesOperation!
+    var operation: CKProcedure<TestCKModifyRecordZonesOperation>!
+
+    override func setUp() {
+        super.setUp()
+        target = TestCKModifyRecordZonesOperation()
+        operation = CKProcedure(operation: target)
+    }
+
+    func test__set_get__recordZonesToSave() {
+        let recordZonesToSave = [ "a-record-zone", "another-record-zone" ]
+        operation.recordZonesToSave = recordZonesToSave
+        XCTAssertNotNil(operation.recordZonesToSave)
+        XCTAssertEqual(operation.recordZonesToSave ?? [], recordZonesToSave)
+        XCTAssertNotNil(target.recordZonesToSave)
+        XCTAssertEqual(target.recordZonesToSave ?? [], recordZonesToSave)
+    }
+
+    func test__set_get__recordZoneIDsToDelete() {
+        let recordZoneIDsToDelete = [ "a-record-zone-id", "another-record-zone-id" ]
+        operation.recordZoneIDsToDelete = recordZoneIDsToDelete
+        XCTAssertNotNil(operation.recordZoneIDsToDelete)
+        XCTAssertEqual(operation.recordZoneIDsToDelete ?? [], recordZoneIDsToDelete)
+        XCTAssertNotNil(target.recordZoneIDsToDelete)
+        XCTAssertEqual(target.recordZoneIDsToDelete ?? [], recordZoneIDsToDelete)
+    }
+
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setModifyRecordZonesCompletionBlock { savedRecordZones, deletedRecordZoneIDs in
+            didExecuteBlock = true
+        }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block() {
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setModifyRecordZonesCompletionBlock { savedRecordZones, deletedRecordZoneIDs in
+            didExecuteBlock = true
+        }
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
+
