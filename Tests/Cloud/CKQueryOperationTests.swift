9a7556086bcc87c48a47989a673bfc6ba6327975
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 21:01:25 -0400

    [OPR-542]: Adds tests for CKQueryOperation

--- Tests/Cloud/CKQueryOperationTests.swift
+++ Tests/Cloud/CKQueryOperationTests.swift
@@ -28,3 +28,85 @@ class TestCKQueryOperation: TestCKDatabaseOperation, CKQueryOperationProtocol, A
         queryCompletionBlock?(cursor, error)
     }
 }
+
+class CKQueryOperationTests: CKProcedureTestCase {
+
+    var target: TestCKQueryOperation!
+    var operation: CKProcedure<TestCKQueryOperation>!
+
+    override func setUp() {
+        super.setUp()
+        target = TestCKQueryOperation()
+        operation = CKProcedure(operation: target)
+    }
+
+    func test__set_get__query() {
+        let query = "a-query"
+        operation.query = query
+        XCTAssertNotNil(operation.query)
+        XCTAssertEqual(operation.query, query)
+        XCTAssertNotNil(target.query)
+        XCTAssertEqual(target.query, query)
+    }
+
+    func test__set_get__cursor() {
+        let cursor = "a-cursor"
+        operation.cursor = cursor
+        XCTAssertNotNil(operation.cursor)
+        XCTAssertEqual(operation.cursor, cursor)
+        XCTAssertNotNil(target.cursor)
+        XCTAssertEqual(target.cursor, cursor)
+    }
+
+    func test__set_get__zoneID() {
+        let zoneID = "a-zone-id"
+        operation.zoneID = zoneID
+        XCTAssertNotNil(operation.zoneID)
+        XCTAssertEqual(operation.zoneID, zoneID)
+        XCTAssertNotNil(target.zoneID)
+        XCTAssertEqual(target.zoneID, zoneID)
+    }
+
+    func test__set_get__recordFetchedBlock() {
+        var setByCompletionBlock = false
+        let block: (String) -> Void = { record in
+            setByCompletionBlock = true
+        }
+        operation.recordFetchedBlock = block
+        XCTAssertNotNil(operation.recordFetchedBlock)
+        target.recordFetchedBlock?("a-record")
+        XCTAssertTrue(setByCompletionBlock)
+    }
+
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setQueryCompletionBlock { cursor in
+            didExecuteBlock = true
+        }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block() {
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setQueryCompletionBlock { cursor in
+            didExecuteBlock = true
+        }
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
