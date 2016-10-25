cc050a5a0cce895f86fe4c64beaec808b3008592
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 21:51:33 -0400

    [OPR-544]: Fix test__cancel_block_procedure() failures
    It was possible for the testing BlockProcedure to finish prior to the
    `block.cancel()` call, which would result in some of the
    BlockProcedures never cancelling (as they had finished first), and
    a failing test.
    Fixed by using a semaphore inside the BlockProcedure’s block.
    Additionally, the test now waits for all BlockProcedures to cancel
    *and* finish.

--- Tests/Stress Tests/BlockProcedureStressTests.swift
+++ Tests/Stress Tests/BlockProcedureStressTests.swift
@@ -13,10 +13,19 @@ class CancelBlockProcedureStessTests: StressTestCase {
     func test__cancel_block_procedure() {
 
         stress(level: .custom(10, 5_000)) { batch, _ in
-            batch.dispatchGroup.enter()
-            let block = BlockProcedure { }
+            batch.dispatchGroup.enter() // enter once for cancel
+            batch.dispatchGroup.enter() // enter once for finish
+            let semaphore = DispatchSemaphore(value: 0)
+            let block = BlockProcedure {
+                // prevent the BlockProcedure from finishing before it is cancelled
+                semaphore.wait()
+            }
             block.addDidCancelBlockObserver { _, _ in
-                batch.dispatchGroup.leave()
+                batch.dispatchGroup.leave() // leave once for cancel
+                semaphore.signal()
+            }
+            block.addDidFinishBlockObserver { _, _ in
+                batch.dispatchGroup.leave() // leave once for finish
             }
             batch.queue.add(operation: block)
             block.cancel()
