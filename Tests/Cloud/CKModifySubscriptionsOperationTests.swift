0097151f728330bf4bd173905bf92d9581a7a0c0
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 20:56:18 -0400

    [OPR-542]: Adds tests for CKModifySubscriptionsOperation

--- Tests/Cloud/CKModifySubscriptionsOperationTests.swift
+++ Tests/Cloud/CKModifySubscriptionsOperationTests.swift
@@ -33,3 +33,64 @@ class TestCKModifySubscriptionsOperation: TestCKDatabaseOperation, CKModifySubsc
     }
 }
 
+class CKModifySubscriptionsOperationTests: CKProcedureTestCase {
+    
+    var target: TestCKModifySubscriptionsOperation!
+    var operation: CKProcedure<TestCKModifySubscriptionsOperation>!
+    
+    override func setUp() {
+        super.setUp()
+        target = TestCKModifySubscriptionsOperation()
+        operation = CKProcedure(operation: target)
+    }
+    
+    func test__set_get__subscriptionsToSave() {
+        let subscriptionsToSave = [ "a-subscription", "another-subscription" ]
+        operation.subscriptionsToSave = subscriptionsToSave
+        XCTAssertNotNil(operation.subscriptionsToSave)
+        XCTAssertEqual(operation.subscriptionsToSave ?? [], subscriptionsToSave)
+        XCTAssertNotNil(target.subscriptionsToSave)
+        XCTAssertEqual(target.subscriptionsToSave ?? [], subscriptionsToSave)
+    }
+    
+    func test__set_get__recordZoneIDsToDelete() {
+        let subscriptionIDsToDelete = [ "a-subscription-id", "another-subscription-id" ]
+        operation.subscriptionIDsToDelete = subscriptionIDsToDelete
+        XCTAssertNotNil(operation.subscriptionIDsToDelete)
+        XCTAssertEqual(operation.subscriptionIDsToDelete ?? [], subscriptionIDsToDelete)
+        XCTAssertNotNil(target.subscriptionIDsToDelete)
+        XCTAssertEqual(target.subscriptionIDsToDelete ?? [], subscriptionIDsToDelete)
+    }
+    
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+    
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setModifySubscriptionsCompletionBlock { savedSubscriptions, deletedSubscriptionIDs in
+            didExecuteBlock = true
+        }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+    
+    func test__error_without_completion_block() {
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+    
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setModifySubscriptionsCompletionBlock { savedSubscriptions, deletedSubscriptionIDs in
+            didExecuteBlock = true
+        }
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
