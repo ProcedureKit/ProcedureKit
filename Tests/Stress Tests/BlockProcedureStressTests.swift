778a3903313ee610e7277088e10051a122af4b66
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 21:54:11 -0400

    [OPR-544]: Added a new stress test for BlockProcedure cancelling & finishing behavior (allowing for possible races between cancelling and finishing)

--- Tests/Stress Tests/BlockProcedureStressTests.swift
+++ Tests/Stress Tests/BlockProcedureStressTests.swift
@@ -31,4 +31,23 @@ class CancelBlockProcedureStessTests: StressTestCase {
             block.cancel()
         }
     }
+
+    func test_cancel_or_finish_block_procedure() {
+
+        // NOTE:
+        // It is possible for a BlockProcedure to finish prior to the call to
+        // `block.cancel()` (depending on timing) and thus not all of the
+        // BlockProcedures created below may be effectively cancelled.
+        // However, all of the BlockProcedures should finish.
+
+        stress(level: .custom(10, 5_000)) { batch, _ in
+            batch.dispatchGroup.enter()
+            let block = BlockProcedure { }
+            block.addDidFinishBlockObserver { _, _ in
+                batch.dispatchGroup.leave()
+            }
+            batch.queue.add(operation: block)
+            block.cancel()
+        }
+    }
 }
