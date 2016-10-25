d34f9e4421377cddc234894c1b39075984068256
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-24 20:55:45 -0400

    [OPR-542]: Adds tests for CKModifyBadgeOperation

--- Tests/Cloud/CKModifyBadgeOperationTests.swift
+++ Tests/Cloud/CKModifyBadgeOperationTests.swift
@@ -28,3 +28,52 @@ class TestCKModifyBadgeOperation: TestCKOperation, CKModifyBadgeOperationProtoco
         modifyBadgeCompletionBlock?(error)
     }
 }
+
+class CKModifyBadgeOperationTests: CKProcedureTestCase {
+
+    var target: TestCKModifyBadgeOperation!
+    var operation: CKProcedure<TestCKModifyBadgeOperation>!
+    var badge: Int!
+
+    override func setUp() {
+        super.setUp()
+        badge = 9
+        target = TestCKModifyBadgeOperation(value: badge)
+        operation = CKProcedure(operation: target)
+    }
+
+    func test__set_get__badgeValue() {
+        let badgeValue = 4
+        operation.badgeValue = badgeValue
+        XCTAssertEqual(operation.badgeValue, badgeValue)
+        XCTAssertEqual(target.badgeValue, badgeValue)
+    }
+
+    func test__success_without_completion_block() {
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__success_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setModifyBadgeCompletionBlock { didExecuteBlock = true }
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block() {
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithoutErrors(operation)
+    }
+
+    func test__error_with_completion_block() {
+        var didExecuteBlock = false
+        operation.setModifyBadgeCompletionBlock { didExecuteBlock = true }
+        target.error = TestError()
+        wait(for: operation)
+        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
