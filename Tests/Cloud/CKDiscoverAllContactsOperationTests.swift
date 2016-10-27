4bda7d3609cc1d032a17e9ceee7bb2d5f517f9d7
Daniel Thorpe <danthorpe@me.com>
2016-10-27 22:50:13 +0100

    [OPR-542]: Adds tests for CKDiscoverAllContactsOperation

--- Tests/Cloud/CKDiscoverAllContactsOperationTests.swift
+++ Tests/Cloud/CKDiscoverAllContactsOperationTests.swift
@@ -7,6 +7,7 @@
 #if !os(tvOS)
 
 import XCTest
+import CloudKit
 import ProcedureKit
 import TestingProcedureKit
 @testable import ProcedureKitCloud
@@ -63,13 +64,65 @@ class CKDiscoverAllContactsOperationTests: CKProcedureTestCase {
 
     func test__error_with_completion_block() {
         var didExecuteBlock = false
-        operation.setDiscoverAllContactsCompletionBlock { _ in
-            didExecuteBlock = true
-        }
+        operation.setDiscoverAllContactsCompletionBlock { _ in didExecuteBlock = true }
         target.error = TestError()
         wait(for: operation)
         XCTAssertProcedureFinishedWithErrors(operation, count: 1)
         XCTAssertFalse(didExecuteBlock)
+    }
+}
+
+class CloudKitProcedureDiscoverAllContactsOperationTests: CKProcedureTestCase {
+
+    var cloudkit: CloudKitProcedure<TestCKDiscoverAllContactsOperation>!
+
+    override func setUp() {
+        super.setUp()
+        cloudkit = CloudKitProcedure(strategy: .immediate) { TestCKDiscoverAllContactsOperation(result: [ "user info" ]) }
+    }
+
+    func test__cancellation() {
+        cloudkit.cancel()
+        wait(for: cloudkit)
+        XCTAssertProcedureCancelledWithoutErrors(cloudkit)
+    }
+
+    func test__success_without_completion_block_set() {
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+    }
+
+    func test__success_with_completion_block_set() {
+        var didSetDiscoveredUserInfo: [TestCKDiscoverAllContactsOperation.DiscoveredUserInfo]? = nil
+        cloudkit.setDiscoverAllContactsCompletionBlock { didSetDiscoveredUserInfo = $0 }
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+        XCTAssertEqual(didSetDiscoveredUserInfo?.first ?? "not user info", "user info")
+    }
+
+    func test__error_without_completion_block_set() {
+        cloudkit = CloudKitProcedure(strategy: .immediate) {
+            let operation = TestCKDiscoverAllContactsOperation(result: [ "user info" ])
+            operation.error = NSError(domain: CKErrorDomain, code: CKError.internalError.rawValue, userInfo: nil)
+            return operation
+        }
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+    }
+
+    func test__error_with_completion_block_set() {
+        cloudkit = CloudKitProcedure(strategy: .immediate) {
+            let operation = TestCKDiscoverAllContactsOperation(result: [ "user info" ])
+            operation.error = NSError(domain: CKErrorDomain, code: CKError.internalError.rawValue, userInfo: nil)
+            return operation
+        }
+
+        var didSetDiscoveredUserInfo: [TestCKDiscoverAllContactsOperation.DiscoveredUserInfo]? = nil
+        cloudkit.setDiscoverAllContactsCompletionBlock { didSetDiscoveredUserInfo = $0 }
+
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithErrors(cloudkit, count: 1)
+        XCTAssertNil(didSetDiscoveredUserInfo)
     }
 }
 
