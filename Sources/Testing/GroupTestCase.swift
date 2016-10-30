a287b7f61aeb8335863300d7d464f4507bf6348c
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-10-30 07:12:57 -0400

    Adds GroupProcedure concurrency tests (#556)
    * Add GroupConcurrencyTestCase
    
    * Add GroupProcedure.maxConcurrentOperationCount tests

--- Sources/Testing/GroupTestCase.swift
+++ Sources/Testing/GroupTestCase.swift
@@ -40,3 +40,42 @@ open class GroupTestCase: ProcedureKitTestCase {
         super.tearDown()
     }
 }
+
+// MARK: - GroupConcurrencyTestCase
+
+open class GroupConcurrencyTestCase: ConcurrencyTestCase {
+
+    public class GroupTestResult: TestResult {
+        public let group: TestGroupProcedure
+
+        public init(group: TestGroupProcedure, procedures: [TrackingProcedure], duration: TimeInterval, registrar: Registrar) {
+            self.group = group
+            super.init(procedures: procedures, duration: duration, registrar: registrar)
+        }
+    }
+
+    public func concurrencyTestGroup(children: Int = 3, withDelayMicroseconds delayMicroseconds: useconds_t = 500000 /* 0.5 seconds */, withName name: String = #function, withTimeout timeout: TimeInterval = 3, withConfigureBlock configure: (TestGroupProcedure) -> Void, withExpectations expectations: Expectations) {
+
+        concurrencyTestGroup(children: children, withDelayMicroseconds: delayMicroseconds, withName: name, withTimeout: timeout,
+            withConfigureBlock: configure,
+            completionBlock: { (results) in
+                XCTAssertResults(results, matchExpectations: expectations)
+        })
+    }
+
+    public func concurrencyTestGroup(children: Int = 3, withDelayMicroseconds delayMicroseconds: useconds_t = 500000 /* 0.5 seconds */, withName name: String = #function, withTimeout timeout: TimeInterval = 3, withConfigureBlock configure: (TestGroupProcedure) -> Void, completionBlock completion: (GroupTestResult) -> Void) {
+
+        let registrar = Registrar()
+        let testProcedures = create(procedures: children, delayMicroseconds: delayMicroseconds, withRegistrar: registrar)
+        let group = TestGroupProcedure(operations: testProcedures)
+
+        configure(group)
+
+        let startTime = CFAbsoluteTimeGetCurrent()
+        wait(for: group, withTimeout: timeout)
+        let endTime = CFAbsoluteTimeGetCurrent()
+        let duration = Double(endTime) - Double(startTime)
+
+        completion(GroupTestResult(group: group, procedures: testProcedures, duration: duration, registrar: registrar))
+    }
+}
