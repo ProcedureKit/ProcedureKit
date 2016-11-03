ebe1a6ebded99aeb4a8cde0a33abfd0d033f3962
Daniel Thorpe <danthorpe@me.com>
2016-11-03 08:51:45 +0000

    [OPR-542]: Adds test for CKFetchRecordZonesOperation

--- Tests/Cloud/CKFetchRecordZonesOperationTests.swift
+++ Tests/Cloud/CKFetchRecordZonesOperationTests.swift
@@ -5,6 +5,7 @@
 //
 
 import XCTest
+import CloudKit
 import ProcedureKit
 import TestingProcedureKit
 @testable import ProcedureKitCloud
@@ -80,3 +81,86 @@ class CKFetchRecordZonesOperationTests: CKProcedureTestCase {
         XCTAssertFalse(didExecuteBlock)
     }
 }
+
+class CloudKitProcedureFetchRecordZonesOperationTests: CKProcedureTestCase {
+    typealias T = TestCKFetchRecordZonesOperation
+    var cloudkit: CloudKitProcedure<T>!
+
+    override func setUp() {
+        super.setUp()
+        cloudkit = CloudKitProcedure(strategy: .immediate) { TestCKFetchRecordZonesOperation() }
+        cloudkit.container = container
+        cloudkit.previousServerChangeToken = token
+        cloudkit.resultsLimit = 10
+        cloudkit.recordZoneIDs = [ "record zone 1 id", "record zone 2 id" ]
+    }
+
+    func test_set_get_container() {
+        cloudkit.container = "I'm a different container!"
+        XCTAssertEqual(cloudkit.container, "I'm a different container!")
+    }
+
+    func test_set_get_previousServerChangeToken() {
+        cloudkit.previousServerChangeToken = "I'm a different token!"
+        XCTAssertEqual(cloudkit.previousServerChangeToken, "I'm a different token!")
+    }
+
+    func test_set_get_resultsLimit() {
+        cloudkit.resultsLimit = 20
+        XCTAssertEqual(cloudkit.resultsLimit, 20)
+    }
+
+    func test_set_get_recordZoneIDs() {
+        cloudkit.recordZoneIDs = [ "record zone 1 id", "record zone 2 id" ]
+        XCTAssertEqual(cloudkit.recordZoneIDs ?? [], [ "record zone 1 id", "record zone 2 id" ])
+    }
+
+    func test__cancellation() {
+        cloudkit.cancel()
+        wait(for: cloudkit)
+        XCTAssertProcedureCancelledWithoutErrors(cloudkit)
+    }
+
+    func test__success_without_completion_block_set() {
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+    }
+
+    func test__success_with_completion_block_set() {
+        var didExecuteBlock = false
+        cloudkit.setFetchRecordZonesCompletionBlock { _ in
+            didExecuteBlock = true
+        }
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block_set() {
+        cloudkit = CloudKitProcedure(strategy: .immediate) {
+            let operation = TestCKFetchRecordZonesOperation()
+            operation.error = NSError(domain: CKErrorDomain, code: CKError.internalError.rawValue, userInfo: nil)
+            return operation
+        }
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+    }
+
+    func test__error_with_completion_block_set() {
+        cloudkit = CloudKitProcedure(strategy: .immediate) {
+            let operation = TestCKFetchRecordZonesOperation()
+            operation.error = NSError(domain: CKErrorDomain, code: CKError.internalError.rawValue, userInfo: nil)
+            return operation
+        }
+
+        var didExecuteBlock = false
+        cloudkit.setFetchRecordZonesCompletionBlock { _ in
+            didExecuteBlock = true
+        }
+
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithErrors(cloudkit, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
+
