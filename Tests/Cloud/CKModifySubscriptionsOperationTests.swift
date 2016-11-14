ad2884fff273cbcc4ea442831a502ee52142f12b
Daniel Thorpe <danthorpe@me.com>
2016-11-14 14:57:53 +0000

    [OPR-542]: Adds test for CKModifySubscriptionsOperation

--- Tests/Cloud/CKModifySubscriptionsOperationTests.swift
+++ Tests/Cloud/CKModifySubscriptionsOperationTests.swift
@@ -5,6 +5,7 @@
 //
 
 import XCTest
+import CloudKit
 import ProcedureKit
 import TestingProcedureKit
 @testable import ProcedureKitCloud
@@ -92,3 +93,95 @@ class CKModifySubscriptionsOperationTests: CKProcedureTestCase {
         XCTAssertFalse(didExecuteBlock)
     }
 }
+
+class CloudKitProcedureModifySubscriptionsOperationTests: CKProcedureTestCase {
+    typealias T = TestCKModifySubscriptionsOperation
+    var cloudkit: CloudKitProcedure<T>!
+
+    override func setUp() {
+        super.setUp()
+        cloudkit = CloudKitProcedure(strategy: .immediate) { TestCKModifySubscriptionsOperation() }
+        cloudkit.container = container
+        cloudkit.subscriptionsToSave = [ "subscription 1" ]
+        cloudkit.subscriptionIDsToDelete = [ "subscription 2 ID" ]
+    }
+
+    func test__set_get_container() {
+        cloudkit.container = "I'm a different container!"
+        XCTAssertEqual(cloudkit.container, "I'm a different container!")
+    }
+
+    func test__set_get_database() {
+        cloudkit.database = "I'm a different database!"
+        XCTAssertEqual(cloudkit.database, "I'm a different database!")
+    }
+
+    func test__set_get_previousServerChangeToken() {
+        cloudkit.previousServerChangeToken = "I'm a different token!"
+        XCTAssertEqual(cloudkit.previousServerChangeToken, "I'm a different token!")
+    }
+
+    func test__set_get_resultsLimit() {
+        cloudkit.resultsLimit = 20
+        XCTAssertEqual(cloudkit.resultsLimit, 20)
+    }
+
+    func test__set_get_subscriptionsToSave() {
+        cloudkit.subscriptionsToSave = [ "subscription 3" ]
+        XCTAssertEqual(cloudkit.subscriptionsToSave ?? [], [ "subscription 3" ])
+    }
+
+    func test__set_get_subscriptionIDsToDelete() {
+        cloudkit.subscriptionIDsToDelete = [ "subscription 2 ID" ]
+        XCTAssertEqual(cloudkit.subscriptionIDsToDelete ?? [], [ "subscription 2 ID" ])
+    }
+
+    func test__cancellation() {
+        cloudkit.cancel()
+        wait(for: cloudkit)
+        XCTAssertProcedureCancelledWithoutErrors(cloudkit)
+    }
+
+    func test__success_without_completion_block_set() {
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+    }
+
+    func test__success_with_completion_block_set() {
+        var didExecuteBlock = false
+        cloudkit.setModifySubscriptionsCompletionBlock { _, _ in
+            didExecuteBlock = true
+        }
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+        XCTAssertTrue(didExecuteBlock)
+    }
+
+    func test__error_without_completion_block_set() {
+        cloudkit = CloudKitProcedure(strategy: .immediate) {
+            let operation = TestCKModifySubscriptionsOperation()
+            operation.error = NSError(domain: CKErrorDomain, code: CKError.internalError.rawValue, userInfo: nil)
+            return operation
+        }
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithoutErrors(cloudkit)
+    }
+
+    func test__error_with_completion_block_set() {
+        cloudkit = CloudKitProcedure(strategy: .immediate) {
+            let operation = TestCKModifySubscriptionsOperation()
+            operation.error = NSError(domain: CKErrorDomain, code: CKError.internalError.rawValue, userInfo: nil)
+            return operation
+        }
+
+        var didExecuteBlock = false
+        cloudkit.setModifySubscriptionsCompletionBlock { _, _ in
+            didExecuteBlock = true
+        }
+
+        wait(for: cloudkit)
+        XCTAssertProcedureFinishedWithErrors(cloudkit, count: 1)
+        XCTAssertFalse(didExecuteBlock)
+    }
+}
+
