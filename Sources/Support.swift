21f181face841936924913866480745bc6a5aeaa
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2016-11-22 09:26:36 -0500

    Fixes Procedure & Support thread-safety (#598)
    * Change Protector.write() to be synchronous (and optionally return a value from the block)
    
    Thread Sanitizer identified a data race caused by the combination of
    the prior asynchronous `write()` and deinitialization.
    
    * Explicitly `import Dispatch` in Support.swift
    
    Fixes an occasional Xcode warning display glitch.
    
    * Fix Protector.read() to no longer require an `escaping` block
    
    * Adds back `Protector.write(_:completion:)`
    
    Although, like the new write(_:) method, this is now synchronous.
    
    * Wrap all properties of Procedure in a Protector to ensure thread-safety
    
    `errors`, `observers`, `directDependencies`, `conditions` (and
    associated methods) are now thread-safe
    
    * Fix comment formatting

--- Sources/Support.swift
+++ Sources/Support.swift
@@ -4,6 +4,8 @@
 //  Copyright © 2016 ProcedureKit. All rights reserved.
 //
 
+import Dispatch
+
 internal func _abstractMethod(file: StaticString = #file, line: UInt = #line) {
     fatalError("Method must be overriden", file: file, line: line)
 }
@@ -24,13 +26,14 @@ extension Dictionary {
 
 protocol ReadWriteLock {
     mutating func read<T>(_ block: () throws -> T) rethrows -> T
-    mutating func write(_ block: @escaping () -> Void, completion: (() -> Void)?)
+    mutating func write_async(_ block: @escaping () -> Void, completion: (() -> Void)?)
+    mutating func write_sync<T>(_ block: () throws -> T) rethrows -> T
 }
 
 extension ReadWriteLock {
 
-    mutating func write(_ block: @escaping () -> Void) {
-        write(block, completion: nil)
+    mutating func write_async(_ block: @escaping () -> Void) {
+        write_async(block, completion: nil)
     }
 }
 
@@ -42,13 +45,20 @@ struct Lock: ReadWriteLock {
         return try queue.sync(execute: block)
     }
 
-    mutating func write(_ block: @escaping () -> Void, completion: (() -> Void)?) {
+    mutating func write_async(_ block: @escaping () -> Void, completion: (() -> Void)?) {
         queue.async(group: nil, flags: [.barrier]) {
             block()
             if let completion = completion {
                 DispatchQueue.main.async(execute: completion)
             }
         }
+    }
+
+    mutating func write_sync<T>(_ block: () throws -> T) rethrows -> T {
+        let result = try queue.sync(flags: [.barrier]) {
+            try block()
+        }
+        return result
     }
 }
 
@@ -65,16 +75,22 @@ public class Protector<T> {
         return read { $0 }
     }
 
-    public func read<U>(_ block: @escaping (T) -> U) -> U {
-        return lock.read { [unowned self] in block(self.ward) }
+    public func read<U>(_ block: (T) -> U) -> U {
+        return lock.read { block(self.ward) }
     }
 
-    public func write(_ block: @escaping (inout T) -> Void) {
-        lock.write({ block(&self.ward) })
+    /// Synchronously modify the protected value
+    ///
+    /// - Returns: The value returned by the `block`, if any. (discardable)
+    @discardableResult public func write<U>(_ block: (inout T) -> U) -> U {
+        return lock.write_sync({ block(&self.ward) })
     }
 
-    public func write(_ block: @escaping (inout T) -> Void, completion: @escaping (() -> Void)) {
-        lock.write({ block(&self.ward) }, completion: completion)
+    // Supports old callers that expect to pass in a completion block
+    // NOTE: Like `write()`, this is synchronous.
+    public func write(_ block: (inout T) -> Void, completion: (() -> Void)) {
+        lock.write_sync({ block(&self.ward) })
+        completion()
     }
 }
 
