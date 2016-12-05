c213c32c70eeca12c443d3489c8c50a19349f234
Andreas Braun <andreas.braun@bornholdtlee.de>
2016-12-05 22:35:53 +0100

    Added conformance to TestableURLSessionTask for URLSessionTaskProgressProtocol

--- Tests/Network/ProcedureKitNetworkTests.swift
+++ Tests/Network/ProcedureKitNetworkTests.swift
@@ -10,6 +10,27 @@ import TestingProcedureKit
 @testable import ProcedureKitNetwork
 
 extension TestableURLSessionTask: URLSessionTaskProtocol, URLSessionDataTaskProtocol, URLSessionDownloadTaskProtocol, URLSessionUploadTaskProtocol { }
+extension TestableURLSessionTask {
+    public func addObserver(_ observer: NSObject, forKeyPath: String, options: NSKeyValueObservingOptions, context: UnsafeMutableRawPointer?) { }
+    public func removeObserver(_ observer: NSObject, forKeyPath: String) { }
+    
+    public var countOfBytesExpectedToReceive: Int64 {
+        return 100
+    }
+    
+    public var countOfBytesReceived: Int64 {
+        return 100
+    }
+    
+    public var countOfBytesSent: Int64 {
+        return 100
+    }
+    
+    public var countOfBytesExpectedToSend: Int64 {
+        return 100
+    }
+}
+
 extension TestableURLSessionTaskFactory: URLSessionTaskFactory { }
 
 class TestSuiteRuns: XCTestCase {
