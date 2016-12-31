d5db060887792cc8624a9246aa75ebb147076b8a
Daniel Thorpe <danthorpe@me.com>
2016-12-31 22:13:47 +0000

    Fixes building using SwiftPM (#663)
    * [OPR-633]: Updates project for SwiftPM
    
    * [OPR-633]: Removes comments from Package.swift
    
    * [OPR-663]: Updates podspecs
    
    * [OPR-663]: Fixes ProcedureKitNetwork building in SwiftPM
    
    * [OPR-663]: Fixes ProcedureKitCloud for SwiftPM
    
    * [OPR-663]: Fixes ProcedureKitLocation for SwiftPM
    
    * [OPR-663]: Fixes ProcedureKitMac for SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,9 +1,65 @@
-//
-//  ProcedureKit
-//
-//  Copyright © 2016 ProcedureKit. All rights reserved.
-//
-
 import PackageDescription
 
-let package = Package(name: "ProcedureKit")
+let package = Package(
+    name: "ProcedureKit",
+
+    targets: [
+
+        /** ProcedureKit libraries */
+        Target(name: "ProcedureKit"),
+
+        Target(
+            name: "ProcedureKitCloud",
+            dependencies: ["ProcedureKit"]),
+
+        Target(
+            name: "ProcedureKitLocation",
+            dependencies: ["ProcedureKit"]),
+
+        Target(
+            name: "ProcedureKitMac",
+            dependencies: ["ProcedureKit"]),
+
+        Target(
+            name: "ProcedureKitNetwork",
+            dependencies: ["ProcedureKit"]),
+
+        /** Test Support library */
+        Target(
+            name: "TestingProcedureKit",
+            dependencies: ["ProcedureKit"]),
+
+        /** Test executables */
+        Target(
+            name: "ProcedureKitTests",
+            dependencies: ["ProcedureKit", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitStressTests",
+            dependencies: ["ProcedureKit", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitCloudTests",
+            dependencies: ["ProcedureKitCloud", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitLocationTests",
+            dependencies: ["ProcedureKitLocation", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitMacTests",
+            dependencies: ["ProcedureKitMac", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitNetworkTests",
+            dependencies: ["ProcedureKitNetwork", "TestingProcedureKit"])
+
+    ],
+
+    exclude: [
+        "Sources/ProcedureKitMobile",
+        "Sources/ProcedureKitTV",
+        "Tests/ProcedureKitMobileTests",
+        "Tests/ProcedureKitTVTests",
+    ]
+)
