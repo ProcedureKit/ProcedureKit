3c0f53c62d06c9a90e17d5b03e0f2fbf8ce054a4
Daniel Thorpe <danthorpe@me.com>
2016-12-31 20:30:57 +0000

    [OPR-633]: Updates project for SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,9 +1,70 @@
-//
-//  ProcedureKit
-//
-//  Copyright © 2016 ProcedureKit. All rights reserved.
-//
-
 import PackageDescription
 
-let package = Package(name: "ProcedureKit")
+let package = Package(
+    name: "ProcedureKit",
+
+    targets: [
+
+        /** ProcedureKit libraries */
+        Target(name: "ProcedureKit"),
+/*
+        Target(
+            name: "ProcedureKitCloud",
+            dependencies: ["ProcedureKit"]),
+        Target(
+            name: "ProcedureKitLocation",
+            dependencies: ["ProcedureKit"]),
+        Target(
+            name: "ProcedureKitMac",
+            dependencies: ["ProcedureKit"]),
+        Target(
+            name: "ProcedureKitNetwork",
+            dependencies: ["ProcedureKit"]),
+*/
+        /** Test Support library */
+        Target(
+            name: "TestingProcedureKit",
+            dependencies: ["ProcedureKit"]),
+
+        /** Test executables */
+        Target(
+            name: "ProcedureKitTests",
+            dependencies: ["ProcedureKit", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitStressTests",
+            dependencies: ["ProcedureKit", "TestingProcedureKit"])
+/*
+         Target(
+            name: "ProcedureKitCloudTests",
+            dependencies: ["ProcedureKit", "ProcedureKitCloud", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitLocationTests",
+            dependencies: ["ProcedureKit", "ProcedureKitLocation", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitMacTests",
+            dependencies: ["ProcedureKit", "ProcedureKitMac", "TestingProcedureKit"]),
+
+         Target(
+            name: "ProcedureKitNetworkTests",
+            dependencies: ["ProcedureKit", "ProcedureKitNetwork", "TestingProcedureKit"])
+*/
+    ],
+
+    exclude: [
+        "Sources/ProcedureKitCloud",
+        "Sources/ProcedureKitLocation",
+        "Sources/ProcedureKitMac",
+        "Sources/ProcedureKitMobile",
+        "Sources/ProcedureKitNetwork",
+        "Sources/ProcedureKitTV",
+        "Tests/ProcedureKitCloudTests",
+        "Tests/ProcedureKitLocationTests",
+        "Tests/ProcedureKitMacTests",
+        "Tests/ProcedureKitMobileTests",
+        "Tests/ProcedureKitNetworkTests",
+        "Tests/ProcedureKitTVTests",
+    ]
+)
