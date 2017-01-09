20913390196dd1220d11e3924256af4145e4f376
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2017-01-08 19:16:04 -0500

    Add CancellableResultProcedure / CancellableBlockProcedure

--- Sources/ProcedureKit/Block.swift
+++ Sources/ProcedureKit/Block.swift
@@ -46,3 +46,28 @@ open class AsyncResultProcedure<Output>: Procedure, OutputProcedure {
 }
 
 open class AsyncBlockProcedure: AsyncResultProcedure<Void> { }
+
+open class CancellableResultProcedure<Output>: Procedure, OutputProcedure {
+
+    /// A block that receives a closure (that returns the current value of `isCancelled`
+    /// for the CancellableResultProcedure), and returns a value (which is set as the
+    /// CancellableResultProcedure's `output`).
+    public typealias ThrowingCancellableOutputBlock = (() -> Bool) throws -> Output
+
+    public var output: Pending<ProcedureResult<Output>> = .pending
+
+    private let block: ThrowingCancellableOutputBlock
+
+    public init(cancellableBlock: @escaping ThrowingCancellableOutputBlock) {
+        self.block = cancellableBlock
+        super.init()
+    }
+
+    open override func execute() {
+        defer { finish(withError: output.error) }
+        do { output = .ready(.success(try block({[unowned self] in return self.isCancelled }))) }
+        catch { output = .ready(.failure(error)) }
+    }
+}
+
+open class CancellableBlockProcedure: CancellableResultProcedure<Void> { }
