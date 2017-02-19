9479d066a04640703acda291672b66d26b3edbce
Sviatoslav Yakymiv <sviatoslav.yakymiv@gmail.com>
2017-02-20 00:59:35 +0200

    AnyProcedure enhancements (#676)
    * 1. Create AnyInputProcedure and AnyOutputProcedure classes.
    2. Replace procedure box with value box.
    
    * Formatting.

--- Sources/ProcedureKit/Any.swift
+++ Sources/ProcedureKit/Any.swift
@@ -6,50 +6,111 @@
 
 import Dispatch
 
-class AnyProcedureBox_<Input, Output>: GroupProcedure, InputProcedure, OutputProcedure {
-    var input: Pending<Input> = .pending
-    var output: Pending<ProcedureResult<Output>> = .pending
+struct ValueBox<Value> {
+
+    typealias Getter = (Void) -> Value
+    typealias Setter = (Value) -> Void
+
+    private let setter: Setter
+    private let getter: Getter
+
+    var value: Value {
+        get {
+            return getter()
+        }
+        set {
+            setter(newValue)
+        }
+    }
+
+    init(getter: @escaping Getter, setter: @escaping Setter) {
+        self.getter = getter
+        self.setter = setter
+    }
 }
 
-class AnyProcedureBox<Base: Procedure>: AnyProcedureBox_<Base.Input, Base.Output> where Base: InputProcedure & OutputProcedure {
+enum ProcedureOutputBoxCreator {
+    static func outputBox<P: OutputProcedure>(for procedure: P) -> ValueBox<Pending<ProcedureResult<P.Output>>> {
+        let getter = { return procedure.output }
+        let setter = { procedure.output = $0 }
+        let valueBox = ValueBox(getter: getter, setter: setter)
+        return valueBox
+    }
+}
 
-    private var base: Base
+enum ProcedureInputBoxCreator {
+    static func inputBox<P: InputProcedure>(for procedure: P) -> ValueBox<Pending<P.Input>> {
+        let getter = { return procedure.input }
+        let setter = { procedure.input = $0 }
+        let valueBox = ValueBox(getter: getter, setter: setter)
+        return valueBox
+    }
+}
 
-    public override var input: Pending<Base.Input> {
-        get { return base.input }
-        set { base.input = newValue }
+public class AnyInputProcedure<Input>: GroupProcedure, InputProcedure {
+    private var inputBox: ValueBox<Pending<Input>>
+    public var input: Pending<Input> {
+        get {
+            return self.inputBox.value
+        }
+        set {
+            self.inputBox.value = newValue
+        }
     }
 
-    public override var output: Pending<ProcedureResult<Base.Output>> {
-        get { return base.output }
-        set { base.output = newValue }
-    }
-
-    public init(dispatchQueue: DispatchQueue? = nil, base: Base) {
-        self.base = base
+    public init<Base: Procedure>(dispatchQueue: DispatchQueue? = nil, _ base: Base) where Base: InputProcedure,
+                                                                                        Input == Base.Input {
+        self.inputBox = ProcedureInputBoxCreator.inputBox(for: base)
         super.init(dispatchQueue: dispatchQueue, operations: [base])
-        log.enabled = false
+        self.log.enabled = false
+    }
+}
+
+public class AnyOutputProcedure<Output>: GroupProcedure, OutputProcedure {
+    private var outputBox: ValueBox<Pending<ProcedureResult<Output>>>
+    public var output: Pending<ProcedureResult<Output>> {
+        get {
+            return self.outputBox.value
+        }
+        set {
+            self.outputBox.value = newValue
+        }
+    }
+
+    public init<Base: Procedure>(dispatchQueue: DispatchQueue? = nil, _ base: Base) where Base: OutputProcedure,
+                                                                                            Output == Base.Output {
+        self.outputBox = ProcedureOutputBoxCreator.outputBox(for: base)
+        super.init(dispatchQueue: dispatchQueue, operations: [base])
+        self.log.enabled = false
     }
 }
 
 public class AnyProcedure<Input, Output>: GroupProcedure, InputProcedure, OutputProcedure {
-    private typealias Erased = AnyProcedureBox_<Input, Output>
-
-    private var erased: Erased
-
-    public var input: Pending<Erased.Input> {
-        get { return erased.input }
-        set { erased.input = newValue }
+    private var inputBox: ValueBox<Pending<Input>>
+    public var input: Pending<Input> {
+        get {
+            return self.inputBox.value
+        }
+        set {
+            self.inputBox.value = newValue
+        }
     }
 
-    public var output: Pending<ProcedureResult<Erased.Output>> {
-        get { return erased.output }
-        set { erased.output = newValue }
+    private var outputBox: ValueBox<Pending<ProcedureResult<Output>>>
+    public var output: Pending<ProcedureResult<Output>> {
+        get {
+            return self.outputBox.value
+        }
+        set {
+            self.outputBox.value = newValue
+        }
     }
 
-    public init<Base: Procedure>(dispatchQueue: DispatchQueue? = nil, _ base: Base) where Base: InputProcedure & OutputProcedure, Output == Base.Output, Input == Base.Input {
-        erased = AnyProcedureBox(dispatchQueue: dispatchQueue, base: base)
-        super.init(dispatchQueue: erased.dispatchQueue, operations: [erased])
+    public init<Base: Procedure>(dispatchQueue: DispatchQueue? = nil, _ base: Base)
+                            where Base: InputProcedure & OutputProcedure, Output == Base.Output, Input == Base.Input {
+        self.inputBox = ProcedureInputBoxCreator.inputBox(for: base)
+        self.outputBox = ProcedureOutputBoxCreator.outputBox(for: base)
+        super.init(dispatchQueue: dispatchQueue, operations: [base])
         log.enabled = false
     }
 }
