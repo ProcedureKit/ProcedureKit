3f114fca8ef268c8b1fb3d462ce3a33b8db96092
Daniel Thorpe <danthorpe@me.com>
2017-06-18 16:48:21 +0100

    [OPR-744]: Removes the trigger for the moment

--- .ci/buildkite/pipeline.sh
+++ .ci/buildkite/pipeline.sh
@@ -7,20 +7,3 @@ steps:
     agents:
       xcode: "$XCODE"
 YAML
-
-if [[ "$BUILDKITE_BUILD_CREATOR" == "Daniel Thorpe" ]]; then
-  cat <<-YAML
-
-  - wait
-
-  -
-    name: "Test CocoaPods Integration"
-    trigger: "tryprocedurekit"
-    build:
-      message: "Testing ProcedureKit Integration via Cocoapods"
-      commit: "HEAD"
-      branch: "cocoapods"
-      env:
-        PROCEDUREKIT_HASH: "$COMMIT"
-YAML
-fi
