a6f14a6fd919407361197717f9029a45354af55e
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2017-07-24 12:56:00 -0400

    Improve TimeoutObserver docs

--- Sources/ProcedureKit/TimeoutObserver.swift
+++ Sources/ProcedureKit/TimeoutObserver.swift
@@ -8,15 +8,19 @@ import Foundation
 import Dispatch
 
 /**
- An observer which will automatically cancel (with an error)
- if it doesn't finish before a time interval is expired.
+ An observer which will automatically cancel (with an error) the `Procedure`
+ to which it is attached if the `Procedure` doesn't finish executing before a
+ time interval is expired.
 
- IMPORTANT:
- This will cancel a Procedure. It is the responsibility
- of the Procedure subclass to handle cancellation as
- appropriate for it to rapidly finish after it is cancelled.
+ The timer starts right before the Procedure's `execute()` function is called
+ (after the Procedure has been started).
 
- See the documentation for `Procedure.cancel()`.
+ - IMPORTANT:
+ This will cancel a `Procedure`. It is the responsibility of the `Procedure`
+ subclass to handle cancellation as appropriate for it to rapidly finish after
+ it is cancelled.
+
+ - See: the documentation for `Procedure.cancel()`
  */
 public struct TimeoutObserver: ProcedureObserver {
 
