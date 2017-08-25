d232d015bc24e5c824e55f208644f474df33b265
Daniel Thorpe <danthorpe@me.com>
2017-08-25 08:24:54 +0100

    [OPR-793]: Adding all steps back

--- .ci/buildkite/pipeline.sh
+++ .ci/buildkite/pipeline.sh
@@ -1,10 +1,97 @@
 #!/bin/bash
 cat <<-YAML
 steps:
-  - name: "ProcedureKit"
-    command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane mac test"
-    agents:
-      xcode: "$XCODE"
+ -
+  name: "Stress Test"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane mac stress_test"
+  agents:
+    queue: "stress-tests"
+    xcode: "$XCODE"    
+ -
+  name: "ProcedureKit"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane mac test"
+  agents:
+    xcode: "$XCODE"
+ -
+  name: "Network"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane mac test_network"
+  agents:
+    xcode: "$XCODE"            
+ -
+  name: "Location"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane mac test_location"
+  agents:
+    xcode: "$XCODE"      
+ -
+  name: "Cloud"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane mac test_cloud"
+  agents:
+    xcode: "$XCODE"
+ -
+  name: "Mac"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane mac test_mac"
+  agents:
+    xcode: "$XCODE"      
+ -
+  name: "ProcedureKit (iOS)"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_ios"
+  agents:
+    queue: "iOS-Simulator"
+    xcode: "$XCODE"
+ -
+  name: "Network (iOS)"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_network_ios"
+  agents:
+    queue: "iOS-Simulator"
+    xcode: "$XCODE"
+ -
+  name: "Location (iOS)"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_location_ios"
+  agents:
+    queue: "iOS-Simulator"
+    xcode: "$XCODE"
+ -
+  name: "Cloud (iOS)"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_cloud_ios"
+  agents:
+    queue: "iOS-Simulator" 
+    xcode: "$XCODE"      
+ -
+  name: "Mobile (iOS)"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_mobile_ios"
+  agents:
+    queue: "iOS-Simulator" 
+    xcode: "$XCODE"      
+ -
+  name: "ProcedureKit: tvOS"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_tvos"
+  agents:
+    iOS-Simulator: true
+    xcode: "$XCODE"
+ -
+  name: "TV"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_tv_tvos"
+  agents:
+    iOS-Simulator: true    
+    xcode: "$XCODE"
+ -
+  name: "Network (tvOS)"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_network_tvos"
+  agents:
+    queue: "iOS-Simulator" 
+    xcode: "$XCODE"
+ -
+  name: "Location (tvOS)"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_location_tvos"
+  agents:
+    queue: "iOS-Simulator" 
+    xcode: "$XCODE"
+ -
+  name: "Cloud (tvOS)"
+  command: "source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby && bundle install --quiet && bundle exec fastlane ios test_cloud_tvos"
+  agents:
+    queue: "iOS-Simulator" 
+    xcode: "$XCODE"
 YAML
 
 if [[ "$BUILDKITE_BUILD_CREATOR" == "Daniel Thorpe" ]]; then
@@ -41,6 +128,8 @@ cat <<-YAML
 
   - label: ":aws: Generate"
     trigger: "procedurekit-documentation"
+    agents:
+      docs: "true"    
     build:
       message: "Generating documentation for ProcedureKit"
       commit: "HEAD"
