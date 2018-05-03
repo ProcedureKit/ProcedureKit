a9031518a8eb9b00d8b03e388fff5d3293b7b5cc
Daniel Thorpe <danthorpe@me.com>
2018-05-03 21:59:23 +0100

    [OPR-834]: Adds BatchProcedure

---
+++ Sources/ProcedureKit/Batch.swift
@@ -0,0 +1,47 @@
+//
+//  ProcedureKit
+//
+//  Copyright © 2018 ProcedureKit. All rights reserved.
+//
+
+import Foundation
+
+open class BatchProcedure<Transform: Procedure>: GroupProcedure, InputProcedure, OutputProcedure where Transform: InputProcedure, Transform: OutputProcedure {
+
+    public typealias Generator = () -> Transform
+
+    /// - returns: The pending input property
+    public var input: Pending<[Transform.Input]> = .pending
+
+    /// - returns: The pending output result property
+    public var output: Pending<ProcedureResult<[Transform.Output]>> = .pending
+
+    public let generator: Generator
+
+    init(dispatchQueue: DispatchQueue? = nil, via generator: @escaping Generator) {
+        self.generator = generator
+        super.init(dispatchQueue: dispatchQueue, operations: [])
+        name = "Batch<\(Transform.self)>"
+    }
+
+    open override func execute() {
+
+        guard let input = input.value else {
+            finish(withResult: .failure(ProcedureKitError.requirementNotSatisfied()))
+            return
+        }
+
+        let batch = input.map { i -> Transform in
+            let transform = generator()
+            transform.input = .ready(i)
+            return transform
+        }
+
+        let gathered = batch.gathered()
+
+        add(children: batch)
+        add(child: gathered)
+
+        super.execute()
+    }
+}
