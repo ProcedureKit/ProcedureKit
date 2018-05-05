658cd6c78fa6364d5a26de3e26a4ef275a2658aa
Daniel Thorpe <danthorpe@me.com>
2018-05-05 21:27:10 +0100

    [OPR-825]: Tweaks the cancellation logic in ReverseGeocodeUserLocation (#835)

--- Sources/ProcedureKitLocation/ReverseGeocodeUserLocation.swift
+++ Sources/ProcedureKitLocation/ReverseGeocodeUserLocation.swift
@@ -42,8 +42,12 @@ open class ReverseGeocodeUserLocationProcedure: GroupProcedure, OutputProcedure 
 
         override func execute() {
 
-            guard let location = location.value, let placemark = placemark.value else {
-                output = .ready(.failure(ProcedureKitError.requirementNotSatisfied()))
+            guard
+                !isCancelled,
+                let location = location.value,
+                let placemark = placemark.value
+            else {
+                finish(withResult: .failure(ProcedureKitError.requirementNotSatisfied()))
                 return
             }
 
@@ -76,18 +80,18 @@ open class ReverseGeocodeUserLocationProcedure: GroupProcedure, OutputProcedure 
 
         reverseGeocodeLocation = ReverseGeocodeProcedure(timeout: timeout).injectResult(from: userLocation)
 
-        finishing.inject(dependency: userLocation) { procedure, userLocation, errors in
+        finishing.inject(dependency: userLocation) { finishing, userLocation, errors in
             guard let location = userLocation.location, errors.isEmpty else {
-                procedure.cancel(withError: ProcedureKitError.dependency(finishedWithErrors: errors)); return
+                finishing.cancel(withError: ProcedureKitError.dependency(finishedWithErrors: errors)); return
             }
-            procedure.location = .ready(location)
+            finishing.location = .ready(location)
         }
 
-        finishing.inject(dependency: reverseGeocodeLocation) { procedure, reverseGeocodeLocation, errors in
+        finishing.inject(dependency: reverseGeocodeLocation) { finishing, reverseGeocodeLocation, errors in
             guard let placemark = reverseGeocodeLocation.placemark, errors.isEmpty else {
-                procedure.cancel(withError: ProcedureKitError.dependency(finishedWithErrors: errors)); return
+                finishing.cancel(withError: ProcedureKitError.dependency(finishedWithErrors: errors)); return
             }
-            procedure.placemark = .ready(placemark)
+            finishing.placemark = .ready(placemark)
         }
 
         super.init(dispatchQueue: dispatchQueue, operations: [userLocation, reverseGeocodeLocation, finishing])
