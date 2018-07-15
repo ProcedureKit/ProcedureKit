7f93af8db10e39c0d49a1cdd7c96de7149e04b77
Daniel Thorpe <danthorpe@me.com>
2018-07-15 12:13:53 +0100

    Add TryProcedureKit CocoaPods project

---
+++ Integrations/CocoaPods/TryProcedureKit/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  ProcedureKit
+//
+//  Copyright © 2016 ProcedureKit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate { }
+
