444cdd7c6588538c1c78cb703313f26418573008
Daniel Thorpe <danthorpe@me.com>
2018-10-17 09:40:57 +0100

    [OPR-889]: Fixes CKFetchDatabaseChangesOperation

--- Tests/ProcedureKitCloudTests/CKFetchDatabaseChangesOperationTests.swift
+++ Tests/ProcedureKitCloudTests/CKFetchDatabaseChangesOperationTests.swift
@@ -4,8 +4,6 @@
 //  Copyright © 2015-2018 ProcedureKit. All rights reserved.
 //
 
-#if !swift(>=4.1)
-
 import XCTest
 import CloudKit
 import ProcedureKit
@@ -113,9 +111,10 @@ class CKFetchDatabaseChangesOperationTests: CKProcedureTestCase {
         operation.setFetchDatabaseChangesCompletionBlock { _, _ in
             didExecuteBlock = true
         }
-        target.error = TestError()
+        let error = TestError()
+        target.error = error
         wait(for: operation)
-        XCTAssertProcedureFinishedWithErrors(operation, count: 1)
+        PKAssertProcedureFinished(operation, withErrors: true)
         XCTAssertFalse(didExecuteBlock)
     }
 }
@@ -207,7 +206,7 @@ class CloudKitProcedureFetchDatabaseChangesOperationTests: CKProcedureTestCase {
     func test__cancellation() {
         cloudkit.cancel()
         wait(for: cloudkit)
-        XCTAssertProcedureCancelledWithoutErrors(cloudkit)
+        PKAssertProcedureCancelled(cloudkit)
     }
 
     func test__success_without_completion_block_set() {
@@ -236,19 +235,18 @@ class CloudKitProcedureFetchDatabaseChangesOperationTests: CKProcedureTestCase {
     }
 
     func test__error_with_completion_block_set() {
+        let error = NSError(domain: CKErrorDomain, code: CKError.internalError.rawValue, userInfo: nil)
         cloudkit = CloudKitProcedure(strategy: .immediate) {
             let operation = TestCKFetchDatabaseChangesOperation()
-            operation.error = NSError(domain: CKErrorDomain, code: CKError.internalError.rawValue, userInfo: nil)
+            operation.error = error
             return operation
         }
-
         var didExecuteBlock = false
         cloudkit.setFetchDatabaseChangesCompletionBlock { _, _ in
             didExecuteBlock = true
         }
-
         wait(for: cloudkit)
-        XCTAssertProcedureFinishedWithErrors(cloudkit, count: 1)
+        PKAssertProcedureFinished(cloudkit, withErrors: true)
         XCTAssertFalse(didExecuteBlock)
     }
 
@@ -295,6 +293,3 @@ class CloudKitProcedureFetchDatabaseChangesOperationTests: CKProcedureTestCase {
     }
     
 }
-
-#endif
-
