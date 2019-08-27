40df8091e9979dfaf916a98fa9943d82ee3a4fe4
Daniel Thorpe <309420+danthorpe@users.noreply.github.com>
2019-08-27 09:10:04 +0100

    Removing all BuildKite steps except for AWS generate docs

--- .ci/buildkite/pipeline.sh
+++ .ci/buildkite/pipeline.sh
@@ -1,82 +1,15 @@
 #!/bin/bash
 cat <<-YAML
+
 steps:
--
-  name: "Stress Test"
-  command: ".ci/scripts/test-stress"
-  retry:
-    automatic: true
-  artifact_paths:
-    - ".ci/results/xcodebuild.log"
-  agents:
-    xcode: "$XCODE"
--
-  name: "macOS"
-  command: ".ci/scripts/test-macos"
-  retry:
-    automatic: true
-  artifact_paths:
-    - ".ci/results/xcodebuild.log"
-  agents:
-    xcode: "$XCODE"
--
-  name: "iOS"
-  command: ".ci/scripts/test-ios"
-  retry:
-    automatic: true
-  artifact_paths:
-    - ".ci/results/xcodebuild.log"
-  agents:
-    xcode: "$XCODE"
--
-  name: "tvOS"
-  command: ".ci/scripts/test-tvos"
-  retry:
-    automatic: true
-  artifact_paths:
-    - ".ci/results/xcodebuild.log"
-  agents:
-    xcode: "$XCODE"
-    
-- wait
 
-- 
-  name: "Test CocoaPods Integration"
-  command: ".ci/scripts/test-cocoapods"  
-  agents:
-    xcode: "$XCODE"
-- 
-  name: "Test SPM Integration"
-  command: ".ci/scripts/test-spm"  
-  agents:
-    xcode: "$XCODE"
-    
-YAML
-
-cat <<-YAML
-
-- wait
-
-YAML
-
-if [[ "$BUILDKITE_BUILD_CREATOR" != "Daniel Thorpe" ]]; then
-cat <<-YAML
-
-- block: "Docs"
-
-YAML
-fi
-
-cat <<-YAML
-
-- 
-  name: ":aws: Generate Docs"
-  trigger: "procedurekit-documentation"
-  build:
-    message: "Generating documentation for ProcedureKit"
-    commit: "HEAD"
-    branch: "master"
-    env:
-      PROCEDUREKIT_HASH: "$COMMIT"
-      PROCEDUREKIT_BRANCH: "$BRANCH"
+  - name: ":aws: Generate Docs"
+    trigger: "procedurekit-documentation"
+    build:
+      message: "Generating documentation for ProcedureKit"
+      commit: "HEAD"
+      branch: "master"
+      env:
+        PROCEDUREKIT_HASH: "$COMMIT"
+        PROCEDUREKIT_BRANCH: "$BRANCH"
 YAML
