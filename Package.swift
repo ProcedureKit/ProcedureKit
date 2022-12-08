d0f5792020c6b0763ad68ac20ed02c34798aeb5f
Stephen Gurnett <Stephen.Gurnett@synchronoss.com>
2022-12-08 10:31:56 +0100

    Remove unused targets from Package.swift

--- Package.swift
+++ Package.swift
@@ -5,28 +5,9 @@ import PackageDescription
 let pkg = Package(name: "ProcedureKit")
 
 pkg.products = [
-    .library(name: "ProcedureKit", targets: ["ProcedureKit"]),
-    .library(name: "ProcedureKitCloud", targets: ["ProcedureKitCloud"]),
-    .library(name: "ProcedureKitCoreData", targets: ["ProcedureKitCoreData"]),
-    .library(name: "ProcedureKitLocation", targets: ["ProcedureKitLocation"]),
-    .library(name: "ProcedureKitMac", targets: ["ProcedureKitMac"]),
-    .library(name: "ProcedureKitNetwork", targets: ["ProcedureKitNetwork"]),
-    .library(name: "TestingProcedureKit", targets: ["TestingProcedureKit"])
+    .library(name: "ProcedureKit", targets: ["ProcedureKit"])
 ]
 
 pkg.targets = [
     .target(name: "ProcedureKit"),
-    .target(name: "ProcedureKitCloud", dependencies: ["ProcedureKit"]),
-    .target(name: "ProcedureKitCoreData", dependencies: ["ProcedureKit"]),    
-    .target(name: "ProcedureKitLocation", dependencies: ["ProcedureKit"]),
-    .target(name: "ProcedureKitMac", dependencies: ["ProcedureKit"]),
-    .target(name: "ProcedureKitNetwork", dependencies: ["ProcedureKit"]),
-    .target(name: "TestingProcedureKit", dependencies: ["ProcedureKit"]),
-    .testTarget(name: "ProcedureKitTests", dependencies: ["ProcedureKit", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitStressTests", dependencies: ["ProcedureKit", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitCloudTests", dependencies: ["ProcedureKitCloud", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitCoreDataTests", dependencies: ["ProcedureKitCoreData", "TestingProcedureKit"]),    
-    .testTarget(name: "ProcedureKitLocationTests", dependencies: ["ProcedureKitLocation", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitMacTests", dependencies: ["ProcedureKitMac", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitNetworkTests", dependencies: ["ProcedureKitNetwork", "TestingProcedureKit"]),
 ]
